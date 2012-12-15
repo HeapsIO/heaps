7ff3ecdddda2db3fa8df3b1e5fe749a287e9163a
ncannasse <ncannasse@motion-twin.com>
2012-12-16 00:58:20 +0100

    autocut optional height

--- h2d/Tile.hx
+++ h2d/Tile.hx
@@ -128,7 +128,8 @@ class Tile {
 		return new Tile(tex, 0, 0, bmp.width, bmp.height);
 	}
 
-	public static function autoCut( bmp : flash.display.BitmapData, size : Int ) {
+	public static function autoCut( bmp : flash.display.BitmapData, width : Int, ?height : Int ) {
+		if( height == null ) height = width;
 		var colorBG = bmp.getPixel32(bmp.width - 1, bmp.height - 1);
 		var tl = new Array();
 		var w = 1, h = 1;
@@ -137,14 +138,14 @@ class Tile {
 		while( h < bmp.height )
 			h <<= 1;
 		var tex = h3d.Engine.getCurrent().mem.allocTexture(w, h);
-		for( y in 0...Std.int(bmp.height / size) ) {
+		for( y in 0...Std.int(bmp.height / height) ) {
 			var a = [];
 			tl[y] = a;
-			for( x in 0...Std.int(bmp.width / size) ) {
-				var sz = isEmpty(bmp, x * size, y * size, size, colorBG);
+			for( x in 0...Std.int(bmp.width / width) ) {
+				var sz = isEmpty(bmp, x * width, y * height, width, height, colorBG);
 				if( sz == null )
 					break;
-				a.push(new Tile(tex,x*size+sz.dx, y*size+sz.dy, sz.w, sz.h, sz.dx, sz.dy));
+				a.push(new Tile(tex,x*width+sz.dx, y*height+sz.dy, sz.w, sz.h, sz.dx, sz.dy));
 			}
 		}
 		if( w != bmp.width || h != bmp.height ) {
@@ -196,11 +197,11 @@ class Tile {
 		return tiles;
 	}
 	
-	static function isEmpty( b : flash.display.BitmapData, px, py, size, bg : UInt ) {
+	static function isEmpty( b : flash.display.BitmapData, px, py, width, height, bg : UInt ) {
 		var empty = true;
-		var xmin = size, ymin = size, xmax = 0, ymax = 0;
-		for( x in 0...size )
-			for( y in 0...size ) {
+		var xmin = width, ymin = height, xmax = 0, ymax = 0;
+		for( x in 0...width )
+			for( y in 0...height ) {
 				var color = b.getPixel32(x+px, y+py);
 				if( color != bg ) {
 					empty = false;
