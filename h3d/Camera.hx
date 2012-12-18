f4d1a6c48c0d32f8cbac55197e4f067493ea81bf
ncannasse <ncannasse@gmail.com>
2012-12-18 18:36:08 +0100

    added right Handed camera option

--- h3d/Camera.hx
+++ h3d/Camera.hx
@@ -10,6 +10,8 @@ class Camera {
 	public var zNear : Float;
 	public var zFar : Float;
 	
+	public var rightHanded : Bool;
+	
 	public var mproj : Matrix;
 	public var mcam : Matrix;
 	public var m : Matrix;
@@ -18,12 +20,13 @@ class Camera {
 	public var up : Vector;
 	public var target : Vector;
 
-	public function new( fov = 60., zoom = 1., ratio = 1.333333, zNear = 0.02, zFar = 4000. ) {
+	public function new( fov = 60., zoom = 1., ratio = 1.333333, zNear = 0.02, zFar = 4000., rightHanded = false ) {
 		this.fov = fov;
 		this.zoom = zoom;
 		this.ratio = ratio;
 		this.zNear = zNear;
 		this.zFar = zFar;
+		this.rightHanded = rightHanded;
 		pos = new Vector(2, 3, 4);
 		up = new Vector(0, 0, 1);
 		target = new Vector(0, 0, 0);
@@ -33,7 +36,7 @@ class Camera {
 	}
 	
 	public function clone() {
-		var c = new Camera(fov, zoom, ratio, zNear, zFar);
+		var c = new Camera(fov, zoom, ratio, zNear, zFar, rightHanded);
 		c.pos = pos.copy();
 		c.up = up.copy();
 		c.target = target.copy();
@@ -44,7 +47,11 @@ class Camera {
 	public function update() {
 		var az = pos.sub(target);
 		az.normalize();
-		var ax = az.cross(up);
+		var ax;
+		if( rightHanded )
+			ax = up.cross(az);
+		else
+			ax = az.cross(up);
 		ax.normalize();
 		if( ax.length() == 0 ) {
 			ax.x = az.y;
@@ -52,22 +59,41 @@ class Camera {
 			ax.z = az.x;
 		}
 		var ay = az.cross(ax);
-		mcam._11 = ax.x;
-		mcam._12 = ay.x;
-		mcam._13 = az.x;
-		mcam._14 = 0;
-		mcam._21 = ax.y;
-		mcam._22 = ay.y;
-		mcam._23 = az.y;
-		mcam._24 = 0;
-		mcam._31 = ax.z;
-		mcam._32 = ay.z;
-		mcam._33 = az.z;
-		mcam._34 = 0;
-		mcam._41 = -ax.dot3(pos);
-		mcam._42 = -ay.dot3(pos);
-		mcam._43 = -az.dot3(pos);
-		mcam._44 = 1;
+		if( rightHanded ) {
+			mcam._11 = ax.x;
+			mcam._12 = ay.x;
+			mcam._13 = -az.x;
+			mcam._14 = 0;
+			mcam._21 = ax.y;
+			mcam._22 = ay.y;
+			mcam._23 = -az.y;
+			mcam._24 = 0;
+			mcam._31 = ax.z;
+			mcam._32 = ay.z;
+			mcam._33 = -az.z;
+			mcam._34 = 0;
+			mcam._41 = -ax.dot3(pos);
+			mcam._42 = -ay.dot3(pos);
+			mcam._43 = az.dot3(pos);
+			mcam._44 = 1;
+		} else {
+			mcam._11 = ax.x;
+			mcam._12 = ay.x;
+			mcam._13 = az.x;
+			mcam._14 = 0;
+			mcam._21 = ax.y;
+			mcam._22 = ay.y;
+			mcam._23 = az.y;
+			mcam._24 = 0;
+			mcam._31 = ax.z;
+			mcam._32 = ay.z;
+			mcam._33 = az.z;
+			mcam._34 = 0;
+			mcam._41 = -ax.dot3(pos);
+			mcam._42 = -ay.dot3(pos);
+			mcam._43 = -az.dot3(pos);
+			mcam._44 = 1;
+		}
 		mproj = makeFrustumMatrix();
 		m.multiply(mcam, mproj);
 	}
@@ -98,11 +124,19 @@ class Camera {
 		var scale = zoom / Math.tan(fov * Math.PI / 360.0);
 		var m = new Matrix();
 		m.zero();
-		m._11 = scale;
-		m._22 = -scale * ratio;
-		m._33 = zFar / (zNear - zFar);
-		m._34 = -1;
-		m._43 = (zNear * zFar) / (zNear - zFar);
+		if( rightHanded ) {
+			m._11 = scale;
+			m._22 = scale * ratio;
+			m._33 = zFar / (zFar - zNear);
+			m._34 = 1;
+			m._43 = -(zNear * zFar) / (zFar - zNear);
+		} else {
+			m._11 = scale;
+			m._22 = -scale * ratio;
+			m._33 = zFar / (zNear - zFar);
+			m._34 = -1;
+			m._43 = (zNear * zFar) / (zNear - zFar);
+		}
 		return m;
 	}
 		
