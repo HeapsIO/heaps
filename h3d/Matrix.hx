0bdfc287f99bdc730adcd823bc1255365b37cdd1
ncannasse <ncannasse@gmail.com>
2012-12-24 15:35:04 +0100

    minor

--- h3d/Matrix.hx
+++ h3d/Matrix.hx
@@ -388,6 +388,10 @@ class Matrix {
 		_41 = a[12]; _42 = a[13]; _43 = a[14]; _44 = a[15];
 	}
 	
+	public function getFloats() {
+		return [_11, _12, _13, _14, _21, _22, _23, _24, _31, _32, _33, _34, _41, _42, _43, _44];
+	}
+	
 	public function toString() {
 		return "MAT=[\n" +
 			"  [ " + Tools.f(_11) + ", " + Tools.f(_12) + ", " + Tools.f(_13) + ", " + Tools.f(_14) + " ]\n" +
