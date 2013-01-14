8572f3ba4340f437fb32cbc4159abc3e7a772d38
ncannasse <ncannasse@gmail.com>
2013-01-14 18:49:44 +0100

    added, only basic math, will later have tween engine

---
+++ h3d/Tween.hx
@@ -0,0 +1,27 @@
+package h3d;
+
+class Tween {
+
+	public static inline var PI = 3.14159265;
+
+	public static inline function lerp( v1 : Float, v2 : Float, k : Float ) {
+		return v1 * (1 - k) + v2 * k;
+	}
+	
+	public static inline function moveTo( v1 : Float, v2 : Float, max : Float ) {
+		var d = v2 - v1;
+		return if( d > -max && d < max ) v2 else v1 + (d < 0 ? -max : max);
+	}
+	
+	public static inline function powTo( v1 : Float, v2 : Float, p : Float, min = 0.1 ) {
+		var d = v2 - v1;
+		return if( d > -min && d < min ) v2 else v1 + d * p;
+	}
+	
+	public static function angleLerp( a1 : Float, a2 : Float, k : Float ) {
+		var da = (a2 - a1) % (PI * 2);
+		if( da > PI ) da -= 2 * PI else if( da <= -PI ) da += 2 * PI;
+		return a1 + da * k;
+	}
+
+}
