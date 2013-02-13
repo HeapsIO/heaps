f51f2bf86947876506148cb65ef366eda30f93e8
Nicolas Cannasse <ncannasse@gmail.com>
2013-02-13 20:56:48 +0100

    fixed (still don't work with dce full)

--- h3d/scene/CustomObject.hx
+++ h3d/scene/CustomObject.hx
@@ -31,8 +31,8 @@ class CustomObject extends Object {
 			ctx.addPass(draw);
 			return;
 		}
-		try Reflect.setField(material.shader,__unprotect__("mproj"),ctx.camera.m) catch( e : Dynamic ) {};
-		try Reflect.setField(material.shader,__unprotect__("mpos"),absPos) catch( e : Dynamic ) {};
+		try Reflect.setProperty(material.shader,__unprotect__("mproj"),ctx.camera.m) catch( e : Dynamic ) {};
+		try Reflect.setProperty(material.shader,__unprotect__("mpos"),absPos) catch( e : Dynamic ) {};
 		ctx.engine.selectMaterial(material);
 		primitive.render(ctx.engine);
 	}
