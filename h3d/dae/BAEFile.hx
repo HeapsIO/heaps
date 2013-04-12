5dd9e8403e3d9f95fb9e01a4d86615423110979f
ncannasse <ncannasse@gmail.com>
2013-04-12 17:00:02 +0200

    removed collada support (no longer supported)

--- h3d/dae/BAEFile.hx
+++
@@ -1,9 +0,0 @@
-package h3d.dae;
-
-class BAEFile extends flash.utils.ByteArray {
-	
-	public function getLibrary() {
-		return new Library(new BAEReader(new haxe.io.BytesInput(haxe.io.Bytes.ofData(this))).read());
-	}
-	
-}
