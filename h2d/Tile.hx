aad25f94d04f940d915a8bdb6d05b98bc533b782
ncannasse <ncannasse@gmail.com>
2013-05-02 16:27:45 +0200

    added width/height parameters on fromColor

--- h2d/Tile.hx
+++ h2d/Tile.hx
@@ -135,23 +135,26 @@ class Tile {
 	}
 	
 
-	static var COLOR_CACHE = new Map<Int,h2d.Tile>();
-	public static function fromColor( color : Int, ?allocPos : h3d.impl.AllocPos ) {
+	static var COLOR_CACHE = new Map<Int,h3d.mat.Texture>();
+	public static function fromColor( color : Int, width = 1, height = 1, ?allocPos : h3d.impl.AllocPos ) {
 		var t = COLOR_CACHE.get(color);
-		if( t != null && t.hasTexture() )
-			return t;
-		var tex = h3d.Engine.getCurrent().mem.allocTexture(1, 1, allocPos);
-		var bmp = haxe.io.Bytes.alloc(4);
-		bmp.set(0, color & 0xFF);
-		bmp.set(1, (color >> 8) & 0xFF);
-		bmp.set(2, (color >> 16) & 0xFF);
-		bmp.set(3, color >>> 24);
-		tex.uploadBytes(bmp);
-		tex.onContextLost = function() {
-			tex.uploadBytes(bmp);
-		};
-		var t = new Tile(tex, 0, 0, 1, 1);
-		COLOR_CACHE.set(color, t);
+		if( t == null || t.isDisposed() ) {
+			t = h3d.Engine.getCurrent().mem.allocTexture(1, 1, allocPos);
+			var bmp = haxe.io.Bytes.alloc(4);
+			bmp.set(0, color & 0xFF);
+			bmp.set(1, (color >> 8) & 0xFF);
+			bmp.set(2, (color >> 16) & 0xFF);
+			bmp.set(3, color >>> 24);
+			t.uploadBytes(bmp);
+			t.onContextLost = function() {
+				t.uploadBytes(bmp);
+			};
+			COLOR_CACHE.set(color, t);
+		}
+		var t = new Tile(t, 0, 0, 1, 1);
+		// scale to size
+		t.width = width;
+		t.height = height;
 		return t;
 	}
 	
