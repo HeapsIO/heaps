8aeb0c073d31d02b2053a0f04e746b047b310792
ncannasse <ncannasse@gmail.com>
2013-05-02 12:21:02 +0200

    added Plan and Ray def and intersection

---
+++ h3d/col/Plan.hx
@@ -0,0 +1,45 @@
+package h3d.col;
+
+@:allow(h3d.col)
+class Plan {
+	
+	var nx : Float;
+	var ny : Float;
+	var nz : Float;
+	var d : Float;
+	
+	inline function new(nx, ny, nz, d) {
+		this.nx = nx;
+		this.ny = ny;
+		this.nz = nz;
+		this.d = d;
+	}
+	
+	public static inline function fromPoints( p0 : Vector, p1 : Vector, p2 : Vector ) {
+		var d1 = p1.sub(p0);
+		var d2 = p2.sub(p0);
+		var n = d1.cross(d2);
+		return new Plan(n.x,n.y,n.z,n.dot3(p0));
+	}
+	
+	public inline function normalize() {
+		var len = 1 / Math.sqrt(nx * nx + ny * ny + nz * nz);
+		nx *= len;
+		ny *= len;
+		nz *= len;
+		d *= len;
+	}
+	
+	public function toString() {
+		return "{" + FMath.fmt(nx) + "," + FMath.fmt(ny) + "," + FMath.fmt(nz) + "," + FMath.fmt(d) + "}";
+	}
+	
+	public inline function distance( p : Vector ) {
+		return nx * p.x + ny * p.y + nz * p.z - d;
+	}
+
+	public static inline function fromNormalPoint( n : Vector, p : Vector ) {
+		return new Plan(n.x,n.y,n.z,n.dot3(p));
+	}
+	
+}
---
+++ h3d/col/Ray.hx
@@ -0,0 +1,52 @@
+package h3d.col;
+
+@:allow(h3d.col)
+class Ray {
+	
+	var px : Float;
+	var py : Float;
+	var pz : Float;
+	var lx : Float;
+	var ly : Float;
+	var lz : Float;
+	
+	inline function new() {
+	}
+	
+	public function normalize() {
+		var l = lx * lx + ly * ly + lz * lz;
+		if( l < FMath.EPSILON ) l = 0 else l = FMath.isqrt(l);
+		lx *= l;
+		ly *= l;
+		lz *= l;
+	}
+	
+	public function toString() {
+		return "{[" + FMath.fmt(px) + "," + FMath.fmt(py) + "," + FMath.fmt(pz) + "],[" + FMath.fmt(lx) + "," + FMath.fmt(ly) + "," + FMath.fmt(lz) + "]}";
+	}
+	
+	public inline function intersect( p : Plan ) : Vector {
+		var d = lx * p.nx + ly * p.ny + lz * p.nz;
+		var nd = p.d - (px * p.nx + py * p.ny + pz * p.nz);
+		// line parallel with plane
+		if( FMath.abs(d) < FMath.EPSILON )
+			return FMath.abs(nd) < FMath.EPSILON ? new Vector(px, py, pz) : null;
+		else {
+			var k = nd / d;
+			return new Vector(px + lx * k, py + ly * k, pz + lz * k);
+		}
+	}
+	
+	public static function fromPoints( p1 : Vector, p2 : Vector ) {
+		var r = new Ray();
+		r.px = p1.x;
+		r.py = p1.y;
+		r.pz = p1.z;
+		r.lx = p2.x - p1.x;
+		r.ly = p2.y - p1.y;
+		r.lz = p2.z - p1.z;
+		return r;
+	}
+	
+	
+}
