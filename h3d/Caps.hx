2d2b2da9eb384039ce509cf4948cb369dade15af
ncannasse <ncannasse@gmail.com>
2013-05-06 15:38:46 +0200

    added isAndroid

--- h3d/Caps.hx
+++ h3d/Caps.hx
@@ -2,10 +2,12 @@ package h3d;
 
 class Caps {
 
-	public static var width(get,null) : Int;
-	public static var height(get,null) : Int;
-	public static var isTouch(get,null) : Bool;
-	public static var isWindowed(get,null) : Bool;
+	public static var width(get,never) : Int;
+	public static var height(get,never) : Int;
+	public static var isTouch(get,never) : Bool;
+	public static var isWindowed(get,never) : Bool;
+	
+	public static var isAndroid(get,never) : Bool;
 
 	static function get_isWindowed() {
 		var p = flash.system.Capabilities.playerType;
@@ -25,5 +27,9 @@ class Caps {
 		var Cap = flash.system.Capabilities;
 		return isWindowed ? flash.Lib.current.stage.stageHeight : Std.int(Cap.screenResolutionX > Cap.screenResolutionY ? Cap.screenResolutionY : Cap.screenResolutionX);
 	}
-
+	
+	static function get_isAndroid() {
+		return flash.system.Capabilities.manufacturer.indexOf('Android') != -1;
+	}
+	
 }
