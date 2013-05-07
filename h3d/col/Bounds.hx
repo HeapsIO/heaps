6bcb82beb1af46418b7edb2515b0a0e8e65cca24
ncannasse <ncannasse@gmail.com>
2013-05-07 18:18:39 +0200

    inline methods, added clone and toString()

--- h3d/col/Bounds.hx
+++ h3d/col/Bounds.hx
@@ -10,7 +10,7 @@ class Bounds {
 	public var yMax : Float;
 	public var zMax : Float;
 	
-	public function new() {
+	public inline function new() {
 		xMin = 1e20;
 		yMin = 1e20;
 		zMin = 1e20;
@@ -19,7 +19,7 @@ class Bounds {
 		zMax = -1e20;
 	}
 	
-	public function add( b : Bounds ) {
+	public inline function add( b : Bounds ) {
 		if( b.xMin < xMin ) xMin = b.xMin;
 		if( b.xMax > xMax ) xMax = b.xMax;
 		if( b.yMin < yMin ) yMin = b.yMin;
@@ -28,13 +28,21 @@ class Bounds {
 		if( b.zMax > zMax ) zMax = b.zMax;
 	}
 
-	public function addPoint( p : Vector ) {
+	public inline function addPoint( p : Vector ) {
 		if( p.x < xMin ) xMin = p.x;
 		if( p.x > xMax ) xMax = p.x;
 		if( p.y < yMin ) yMin = p.y;
 		if( p.y > yMax ) yMax = p.y;
 		if( p.z < zMin ) zMin = p.z;
 		if( p.z > zMax ) zMax = p.z;
+	}
+	
+	public inline function getMin() {
+		return new Vector(xMin, yMin, zMin);
+	}
+
+	public inline function getMax() {
+		return new Vector(xMax, yMax, zMax);
 	}
 	
 	public inline function reset() {
@@ -46,4 +54,18 @@ class Bounds {
 		zMax = -1e20;
 	}
 	
+	public inline function clone() {
+		var b = new Bounds();
+		b.xMin = xMin;
+		b.yMin = yMin;
+		b.zMin = zMin;
+		b.xMax = xMax;
+		b.yMax = yMax;
+		b.zMax = zMax;
+		return b;
+	}
+	
+	public function toString() {
+		return "{" + getMin() + "," + getMax() + "}";
+	}
 }
