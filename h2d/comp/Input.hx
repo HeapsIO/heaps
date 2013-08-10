fe54856b1b764cdbbccb86f33ca4599963461f8e
Nicolas Cannasse <ncannasse@gmail.com>
2013-08-10 15:33:53 +0200

    fix for Key change

--- h2d/comp/Input.hx
+++ h2d/comp/Input.hx
@@ -1,4 +1,5 @@
 package h2d.comp;
+import hxd.Key;
 
 @:access(h2d.comp.Input.scene)
 class Input extends Component {
@@ -54,7 +55,7 @@ class Input extends Component {
 					value = value.substr(0, cursorPos) + value.substr(cursorPos + 1);
 					onChange(value);
 					return;
-				case Key.BACK:
+				case Key.BACKSPACE:
 					value = value.substr(0, cursorPos - 1) + value.substr(cursorPos);
 					cursorPos--;
 					onChange(value);
