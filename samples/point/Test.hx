f7e9c611aed35173cdbdbbc712316acbc4c840fa
David Elahee <david.elahee@gmail.com>
2013-12-05 11:56:11 +0100

    finalized point sample

--- samples/point/Test.hx
+++ samples/point/Test.hx
@@ -1,5 +1,4 @@
 import flash.Lib;
-import h3d.impl.Shaders.LineShader;
 import h3d.impl.Shaders.PointShader;
 import h3d.mat.Material;
 import h3d.mat.Texture;
@@ -10,9 +9,6 @@ import haxe.CallStack;
 import haxe.io.Bytes;
 import hxd.BitmapData;
 import hxd.Pixels;
-import hxd.res.Embed;
-import hxd.res.EmbedFileSystem;
-import hxd.res.LocalFileSystem;
 import hxd.System;
 
 class PointMaterial extends Material{
@@ -30,7 +26,7 @@ class PointMaterial extends Material{
 	
 	override function setup( ctx : h3d.scene.RenderContext ) {
 		super.setup(ctx);
-		pshader.mproj = ctx.engine.getShaderProjection();
+		pshader.mproj = ctx.engine.curProjMatrix;
 	}
 	
 	public inline function get_delta() return pshader.delta;
@@ -76,34 +72,8 @@ class Test {
 		addPoint(new Vector(0, 0, 0), 0xFFFFFFFF );
 		addPoint(new Vector(1, 0, 0), 0xFFff0000 );
 		
-		function onLoaded( bmp : hxd.BitmapData) {
-			var tex :Texture = Texture.fromBitmap( bmp);
-			var mat = new h3d.mat.MeshMaterial(tex);
-			mat.culling = None;
-			
-			mat.lightSystem = {
-				ambient : new h3d.Vector(0.5, 0.5, 0.5),
-				
-				dirs : [ 
-					{ dir : new h3d.Vector( -0.3, -0.5, -1), color : new h3d.Vector(1, 0.5, 0.5) },
-					{ dir : new h3d.Vector( -0.3, -0.5, 1), color : new h3d.Vector(0.0, 0, 1.0) }
-				],
-				points : [{ pos : new h3d.Vector(1.5,0,0), color : new h3d.Vector(0,1,0), att : new h3d.Vector(0,0,1) }],
-				
-			};
-			
-			update();
-			hxd.System.setLoop(update);
-		}
-		
-		#if sys
-		if ( lfs.exists("hxlogo.png")) {
-			lfs.get("hxlogo.png").loadBitmap(onLoaded);
-		}
-		#else 
-			//erk
-			onLoaded(hxd.Res.hxlogo.toBitmap());
-		#end
+		update();
+		hxd.System.setLoop(update);
 	}
 	
 	function update() {	
@@ -114,30 +84,13 @@ class Test {
 		engine.render(scene);
 	}
 	
-	
-	static var lfs: LocalFileSystem;
 	static function main() {
 		
 		#if flash
 		haxe.Log.setColor(0xFF0000);
 		#end
 		
-		#if flash
-			EmbedFileSystem.init();
-		#else
-			lfs = new hxd.res.LocalFileSystem('res');
-			var it = lfs.getRoot().iterator();//bugs
-			var e:Dynamic = null;
-			do
-			{
-				e = it.next();
-				trace( 'detecting file ${e.name}');
-			}
-			while ( it.hasNext() );
-		#end
 		new Test();
-		
-		
 	}
 	
 }
