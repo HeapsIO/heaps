7deef733b364f9d1f88eebf1c0c0efb54d7cf314
Ivan Tivonenko <dark@aerohills.com>
2014-01-05 19:55:50 +0200

    embed font only in flash target

--- samples/comps/Comps.hx
+++ samples/comps/Comps.hx
@@ -9,7 +9,9 @@ class Comps {
 		engine.init();
 		// make sure that arial.ttf is inside the current class path (remove "true" to get errors)
 		// emebedding the font will greatly improve visibility on flash (might be required on some targets)
+        #if flash
 		hxd.res.Embed.embedFont("arial.ttf", true);
+        #end
 	}
 	
 	function init() {
