6f2f5f593514610afbe60f34b00962624c8c3608
david.elahee@gmail.com <david.elahee@gmail.com>
2014-08-05 11:33:16 +0200

    compare problem for bounds

--- h2d/Graphics.hx
+++ h2d/Graphics.hx
@@ -302,7 +302,7 @@ class Graphics extends Drawable {
 		if( x < xMin ) xMin = x;
 		if( y < yMin ) yMin = y;
 		if( x > xMax ) xMax = x;
-		if( y < yMax ) yMax = y;
+		if( y > yMax ) yMax = y;
 		if( doFill ) {
 			var p = new GraphicsPoint(x, y);
 			p.id = pindex++;
