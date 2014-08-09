968bffc6fc6d4974898cc1491330a8bd826f0b51
Nicolas Cannasse <ncannasse@gmail.com>
2014-08-09 21:20:59 +0200

    fixes compilation issues with js

--- hxd/ByteConversions.hx
+++ hxd/ByteConversions.hx
@@ -7,6 +7,8 @@ import haxe.io.Bytes;
  */
 class ByteConversions{
 
+#if (flash || openfl)
+
 	public static function byteArrayToBytes( v: flash.utils.ByteArray ) : haxe.io.Bytes {
 		return
 		#if flash
@@ -19,18 +21,12 @@ class ByteConversions{
 			b;
 		};
 		#elseif (openfl)
-		v; 
+		v;
 		#else
 		throw "unsupported on this platform";
 		#end
 	}
-	
-	#if js
-	public static function arrayBufferToBytes( v : js.html.ArrayBuffer ) : haxe.io.Bytes{
-		return byteArrayToBytes(flash.utils.ByteArray.nmeOfBuffer(v));
-	}
-	#end
-		
+
 	public static function bytesToByteArray( v: haxe.io.Bytes ) :  flash.utils.ByteArray {
 		#if flash
 		return v.getData();
@@ -40,4 +36,13 @@ class ByteConversions{
 		throw "unsupported on this platform";
 		#end
 	}
+
+	#if js
+	public static function arrayBufferToBytes( v : js.html.ArrayBuffer ) : haxe.io.Bytes{
+		return byteArrayToBytes(flash.utils.ByteArray.nmeOfBuffer(v));
+	}
+	#end
+
+#end
+
 }
