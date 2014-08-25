85a9a5bb00722285f27f3e3e79c0f0e8aaff2ead
Nicolas Cannasse <ncannasse@gmail.com>
2014-08-25 16:23:41 +0200

    fixed example

--- samples/2d/Main.hx
+++ samples/2d/Main.hx
@@ -1,13 +1,13 @@
 class Main extends hxd.App {
-	
+
 	var spr : h2d.Sprite;
-	
+
 	override function init() {
 		var tile = hxd.Res.hxlogo.toTile();
 		spr = new h2d.Sprite(s2d);
 		spr.x = s2d.width >> 1;
 		spr.y = s2d.height >> 1;
-		
+
 		for( i in 0...15 ) {
 			var bmp = new h2d.Bitmap(tile, spr);
 			bmp.x = Math.cos(i * Math.PI / 8) * 100 - (tile.width>>1);
@@ -17,10 +17,10 @@ class Main extends hxd.App {
 
 		//var font = hxd.Res.CustomFont.build(32, { antiAliasing : true } );
 		var font = hxd.res.FontBuilder.getFont("Arial", 32);
-		
+
 		var tf = new h2d.Text(font, s2d);
 		tf.textColor = 0xFFFFFF;
-		tf.dropShadow = { x : 0.5, y : 0.5, color : 0xFF0000, alpha : 0.8 };
+		tf.dropShadow = { dx : 0.5, dy : 0.5, color : 0xFF0000, alpha : 0.8 };
 		tf.text = "Héllò h2d !";
 		tf.x = 20;
 		tf.y = 450;
@@ -31,20 +31,20 @@ class Main extends hxd.App {
 
 		var tf = new h2d.Text(font, s2d);
 		tf.textColor = 0xFFFFFF;
-		tf.dropShadow = { x : 0.5, y : 0.5, color : 0xFF0000, alpha : 0.8 };
+		tf.dropShadow = { dx : 0.5, dy : 0.5, color : 0xFF0000, alpha : 0.8 };
 		tf.text = "Héllò h2d !";
-		
+
 		tf.x = 20;
 		tf.scale(7);
 	}
-	
+
 	override function update(dt:Float) {
 		spr.rotation += 0.01 * dt;
 	}
-	
+
 	static function main() {
 		hxd.Res.initEmbed();
 		new Main();
 	}
-	
+
 }
