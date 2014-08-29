122618f035daa27124a71deecb42e6ddf1683c43
Nicolas Cannasse <ncannasse@gmail.com>
2014-08-29 22:12:36 +0200

    fixed the cube default tcoords so there is less flip

--- h3d/prim/Cube.hx
+++ h3d/prim/Cube.hx
@@ -34,18 +34,18 @@ class Cube extends Polygon {
 	override function addUVs() {
 		unindex();
 
-		var z = new UV(0, 0);
-		var x = new UV(1, 0);
-		var y = new UV(0, 1);
-		var o = new UV(1, 1);
+		var z = new UV(0, 1);
+		var x = new UV(1, 1);
+		var y = new UV(0, 0);
+		var o = new UV(1, 0);
 
 		uvs = [
 			z, x, o,
 			z, o, y,
 			x, z, y,
 			x, y, o,
-			z, x, o,
-			z, o, y,
+			x, z, y,
+			x, y, o,
 			z, o, x,
 			z, y, o,
 			x, y, z,
