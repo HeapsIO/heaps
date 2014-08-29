6d1c5ca23f18e9b8b4a175020150d1e9c815fdf9
Nicolas Cannasse <ncannasse@gmail.com>
2014-08-29 23:09:13 +0200

    embed font

--- samples/comps/Comps.hx
+++ samples/comps/Comps.hx
@@ -1,17 +1,17 @@
 class Comps {
-	
+
 	var engine : h3d.Engine;
 	var scene : h2d.Scene;
-	
+
 	function new() {
 		engine = new h3d.Engine();
 		engine.onReady = init;
 		engine.init();
 		// make sure that arial.ttf is inside the current class path (remove "true" to get errors)
-		// emebedding the font will greatly improve visibility on flash (might be required on some targets)
-		//hxd.res.Embed.embedFont("arial.ttf", true);
+		// embedding the font will greatly improve visibility on flash (might be required on some targets)
+		hxd.res.Embed.embedFont("Arial.ttf", true);
 	}
-	
+
 	function init() {
 		hxd.System.setLoop(update);
 		scene = new h2d.Scene();
@@ -19,14 +19,14 @@ class Comps {
 		scene.addChild(document);
 		engine.onResized = function() document.setStyle(null);
 	}
-	
+
 	function update() {
 		engine.render(scene);
 		scene.checkEvents();
 	}
-	
+
 	static function main() {
 		new Comps();
 	}
-	
+
 }
