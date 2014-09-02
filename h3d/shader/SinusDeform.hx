cec8b1bcfff543ab37b3fa364e225ad5167edf7a
ncannasse <ncannasse@gmail.com>
2014-09-02 14:16:33 +0200

    added SinusDeform

---
+++ h3d/shader/SinusDeform.hx
@@ -0,0 +1,19 @@
+package h3d.shader;
+
+class SinusDeform extends hxsl.Shader {
+
+	static var SRC = {
+
+		@param var time : Float;
+		@param var frequency : Float;
+		@param var amplitude : Float;
+
+		var calculatedUV : Vec2;
+
+		function fragment() {
+			calculatedUV.x += sin(calculatedUV.x * frequency + time) * amplitude;
+		}
+
+	};
+
+}
