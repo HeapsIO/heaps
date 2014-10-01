e75cee93f0d6c1eee7076df3f94f11dffcefdd69
ncannasse <ncannasse@gmail.com>
2014-10-01 18:51:32 +0200

    fixed HtmlText textColor and dropShadow, added support for <img>

--- h2d/HtmlText.hx
+++ h2d/HtmlText.hx
@@ -2,8 +2,39 @@ package h2d;
 
 class HtmlText extends Text {
 
+	var images : Array<Bitmap> = [];
+
+	override function draw(ctx:RenderContext) {
+		if( dropShadow != null ) {
+			var oldX = absX, oldY = absY;
+			absX += dropShadow.dx * matA + dropShadow.dy * matC;
+			absY += dropShadow.dx * matB + dropShadow.dy * matD;
+			var old = this.colorMatrix;
+			this.colorMatrix = new h3d.Matrix();
+			this.colorMatrix.zero();
+			this.colorMatrix._41 = ((dropShadow.color >> 16) & 0xFF) / 255;
+			this.colorMatrix._42 = ((dropShadow.color >> 8) & 0xFF) / 255;
+			this.colorMatrix._43 = (dropShadow.color & 0xFF) / 255;
+			this.colorMatrix._44 = dropShadow.alpha;
+			glyphs.drawWith(ctx, this);
+			this.colorMatrix = old;
+			absX = oldX;
+			absY = oldY;
+			calcAbsPos();
+		}
+		glyphs.drawWith(ctx,this);
+	}
+
+	public dynamic function loadImage( url : String ) : Tile {
+		return null;
+	}
+
 	override function initGlyphs( text : String, rebuild = true, ?lines : Array<Int> ) {
-		if( rebuild ) glyphs.reset();
+		if( rebuild ) {
+			glyphs.reset();
+			for( i in images ) i.remove();
+			images = [];
+		}
 		glyphs.setDefaultColor(textColor);
 		var x = 0, y = 0, xMax = 0;
 		function loop( e : Xml ) {
@@ -24,6 +55,16 @@ class HtmlText extends Text {
 					if( x > xMax ) xMax = x;
 					x = 0;
 					y += font.lineHeight;
+				case "img":
+					var i = loadImage(e.get("src"));
+					if( i == null ) i = Tile.fromColor(0xFF00FF, 8, 8);
+					if( rebuild ) {
+						var b = new Bitmap(i, this);
+						b.x = x;
+						b.y = y + font.baseLine - i.height;
+						images.push(b);
+					}
+					x += i.width + letterSpacing;
 				default:
 				}
 				for( child in e )
@@ -46,6 +87,11 @@ class HtmlText extends Text {
 		for( e in Xml.parse(text) )
 			loop(e);
 		return { width : x > xMax ? x : xMax, height : x > 0 ? y + font.lineHeight : y };
+	}
+
+	override function set_textColor(c) {
+		this.textColor = c;
+		return c;
 	}
 
 }
