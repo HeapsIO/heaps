dae5bfe6fb3a073e11d9840c2337060ae106757b
ncannasse <ncannasse@gmail.com>
2014-10-24 11:25:04 +0200

    added uvDelta

--- h3d/pass/Copy.hx
+++ h3d/pass/Copy.hx
@@ -4,9 +4,10 @@ private class CopyShader extends h3d.shader.ScreenShader {
 
 	static var SRC = {
 		@param var texture : Sampler2D;
+		@param var uvDelta : Vec2;
 
 		function fragment() {
-			output.color = texture.get(input.uv);
+			output.color = texture.get(input.uv + uvDelta);
 		}
 	}
 }
@@ -17,19 +18,20 @@ class Copy extends ScreenFx<CopyShader> {
 		super(new CopyShader());
 	}
 
-	public function apply( from, to, ?blend : h3d.mat.BlendMode ) {
+	public function apply( from, to, ?blend : h3d.mat.BlendMode, ?uvDelta : h3d.Vector ) {
 		pass.setBlendMode(blend == null ? None : blend);
-		engine.setTarget(to);
+		var prev = engine.setTarget(to);
 		shader.texture = from;
+		if( uvDelta == null ) shader.uvDelta.set(0, 0) else shader.uvDelta.set(uvDelta.x, uvDelta.y);
 		render();
 		shader.texture = null;
-		engine.setTarget(null);
+		engine.setTarget(prev);
 	}
 
 	static var inst : Copy;
-	public static function run( from : h3d.mat.Texture, to : h3d.mat.Texture, ?blend ) {
+	public static function run( from : h3d.mat.Texture, to : h3d.mat.Texture, ?blend, ?uvDelta : h3d.Vector ) {
 		if( inst == null ) inst = new Copy();
-		return inst.apply(from, to, blend);
+		return inst.apply(from, to, blend, uvDelta);
 	}
 
 }
