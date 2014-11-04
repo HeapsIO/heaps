b32d8be55bd4bab29e0c9ca7334da181095ac9d9
ncannasse <ncannasse@gmail.com>
2014-11-04 14:36:11 +0100

    fixed point light shader

--- h3d/shader/PointLight.hx
+++ h3d/shader/PointLight.hx
@@ -9,21 +9,22 @@ class PointLight extends hxsl.Shader {
 		var transformedPosition : Vec3;
 		var transformedNormal : Vec3;
 		@param var color : Vec3;
-		@param var params : Vec3;
+		@param var params : Vec3; // [constant, linear, quadratic]
 		@param var lightPosition : Vec3;
 
 		function vertex() {
 			var dvec = lightPosition - transformedPosition;
 			var dist2 = dvec.dot(dvec);
 			var dist = dist2.sqrt();
-			lightColor.rgb += color * (transformedNormal.dot(dvec).max(0.) / vec3(1, dist, dist2).dot(params));
+			// TN . DVEC is already multiplied by dist
+			lightColor.rgb += color * (transformedNormal.dot(dvec).max(0.) / vec3(dist, dist2, dist * dist2).dot(params));
 		}
 
 		function fragment() {
 			var dvec = lightPosition - transformedPosition;
 			var dist2 = dvec.dot(dvec);
 			var dist = dist2.sqrt();
-			lightPixelColor.rgb += color * (transformedNormal.dot(dvec).max(0.) / vec3(1, dist, dist2).dot(params));
+			lightPixelColor.rgb += color * (transformedNormal.dot(dvec).max(0.) / vec3(dist, dist2, dist * dist2).dot(params));
 		}
 
 	};
