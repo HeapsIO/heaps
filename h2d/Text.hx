c105fa37037289b1a2b7c952aa2346659bb843be
ncannasse <ncannasse@gmail.com>
2014-12-07 08:50:12 +0100

    only apply alpha to drop shadow

--- h2d/Text.hx
+++ h2d/Text.hx
@@ -74,9 +74,6 @@ class Text extends Drawable {
 			var oldB = color.b;
 			var oldA = color.a;
 			color.setColor(dropShadow.color);
-			color.r *= oldR;
-			color.g *= oldG;
-			color.b *= oldB;
 			color.a = dropShadow.alpha * oldA;
 			glyphs.drawWith(ctx, this);
 			absX = oldX;
