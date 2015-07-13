cf4891cbe2fd8ac34c46140204a276d17328177f
ncannasse <ncannasse@gmail.com>
2015-07-13 18:44:59 +0200

    added vars access

--- h2d/filter/Bloom.hx
+++ h2d/filter/Bloom.hx
@@ -3,6 +3,8 @@ package h2d.filter;
 class Bloom extends Blur {
 
 	var bloom : h3d.pass.ScreenFx<h3d.shader.Bloom>;
+	public var amount(get, set) : Float;
+	public var power(get, set) : Float;
 
 	public function new( power = 2., amount = 1., quality = 2, passes = 1, sigma = 1 ) {
 		super(quality, passes, sigma);
@@ -11,6 +13,11 @@ class Bloom extends Blur {
 		bloom.shader.amount = amount;
 		@:privateAccess bloom.pass.blend(One, One);
 	}
+
+	inline function get_amount() return bloom.shader.amount;
+	inline function set_amount(v) return bloom.shader.amount = v;
+	inline function get_power() return bloom.shader.power;
+	inline function set_power(v) return bloom.shader.power = v;
 
 	override function draw( ctx : RenderContext, t : h2d.Tile ) {
 		var dst = ctx.textures.allocTarget("dest", ctx, t.width, t.height, false);
