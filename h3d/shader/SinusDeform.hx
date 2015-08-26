831ff7037d285e4118013d91fcae4b1020cb5c39
ncannasse <ncannasse@gmail.com>
2015-08-26 12:45:41 +0200

    SinusDeform now with parameters and uses global time

--- h3d/shader/SinusDeform.hx
+++ h3d/shader/SinusDeform.hx
@@ -4,16 +4,24 @@ class SinusDeform extends hxsl.Shader {
 
 	static var SRC = {
 
-		@param var time : Float;
+		@global var time : Float;
+		@param var speed : Float;
 		@param var frequency : Float;
 		@param var amplitude : Float;
 
 		var calculatedUV : Vec2;
 
 		function fragment() {
-			calculatedUV.x += sin(calculatedUV.y * frequency + time) * amplitude;
+			calculatedUV.x += sin(calculatedUV.y * frequency + time * speed) * amplitude;
 		}
 
 	};
+
+	public function new( frequency = 10., amplitude = 0.01, speed = 1. ) {
+		super();
+		this.frequency = frequency;
+		this.amplitude = amplitude;
+		this.speed = speed;
+	}
 
 }
