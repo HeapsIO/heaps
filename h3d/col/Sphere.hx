3fe3dd091092fc64c29a910fee77ed9ccf7609b0
bstouls <vallandre@hotmail.com>
2016-02-08 18:43:37 +0100

    add rayIntersection()

--- h3d/col/Sphere.hx
+++ h3d/col/Sphere.hx
@@ -1,6 +1,6 @@
 package h3d.col;
 
-class Sphere {
+class Sphere implements RayCollider {
 
 	public var x : Float;
 	public var y : Float;
@@ -16,6 +16,26 @@ class Sphere {
 
 	public inline function getCenter() {
 		return new Point(x, y, z);
+	}
+
+	public function rayIntersection( r : Ray, ?p : Point ) : Null<Point> {
+		var r2 = this.r * this.r;
+		var px = r.px + r.lx;
+		var py = r.py + r.ly;
+		var pz = r.pz + r.lz;
+
+		var a = r.lx * r.lx + r.ly * r.ly + r.lz * r.lz;
+		var b = 2 * r.lx * (x - px) +  2 * r.ly * (y - py) +  2 * r.lz * (z - pz);
+		var c = (x * x + y * y + z * z) + (px * px + py * py + pz * pz) - 2 * (x * px + y * py + z * pz) - r2;
+
+		var d = b * b - 4 * a * c;
+		if( d < 0 )	return null;
+
+		d = Math.sqrt(d);
+		var t = ( -b + d) / (2 * a);
+		if( p == null ) p = new Point();
+		p.set(px - t * r.lx , py - t * r.ly, pz - t * r.lz);
+		return p;
 	}
 
 	public function inFrustum( mvp : Matrix ) {
