763c890fd357c6584dbcac38e0061aee5150e9cb
ncannasse <ncannasse@gmail.com>
2016-03-31 12:34:50 +0200

    Copy can take a custom pass

--- h3d/pass/Copy.hx
+++ h3d/pass/Copy.hx
@@ -4,10 +4,15 @@ private class CopyShader extends h3d.shader.ScreenShader {
 
 	static var SRC = {
 		@param var texture : Sampler2D;
-		@param var uvDelta : Vec2;
+
+		var pixelColor : Vec4;
+
+		function __init__fragment() {
+			pixelColor = texture.get(input.uv);
+		}
 
 		function fragment() {
-			output.color = texture.get(input.uv + uvDelta);
+			output.color = pixelColor;
 		}
 	}
 }
@@ -18,20 +23,29 @@ class Copy extends ScreenFx<CopyShader> {
 		super(new CopyShader());
 	}
 
-	public function apply( from, to, ?blend : h3d.mat.BlendMode, ?uvDelta : h3d.Vector ) {
-		pass.setBlendMode(blend == null ? None : blend);
+	public function apply( from, to, ?blend : h3d.mat.BlendMode, ?customPass : h3d.mat.Pass ) {
 		engine.pushTarget(to);
 		shader.texture = from;
-		if( uvDelta == null ) shader.uvDelta.set(0, 0) else shader.uvDelta.set(uvDelta.x, uvDelta.y);
-		render();
+		if( customPass != null ) {
+			var old = pass;
+			pass = customPass;
+			pass.addShaderAt(shader, 0);
+			if( blend != null ) pass.setBlendMode(blend);
+			render();
+			pass.removeShader(shader);
+			pass = old;
+		} else {
+			pass.setBlendMode(blend == null ? None : blend);
+			render();
+		}
 		shader.texture = null;
 		engine.popTarget();
 	}
 
 	static var inst : Copy;
-	public static function run( from : h3d.mat.Texture, to : h3d.mat.Texture, ?blend, ?uvDelta : h3d.Vector ) {
+	public static function run( from : h3d.mat.Texture, to : h3d.mat.Texture, ?blend, ?pass : h3d.mat.Pass ) {
 		if( inst == null ) inst = new Copy();
-		return inst.apply(from, to, blend, uvDelta);
+		return inst.apply(from, to, blend, pass);
 	}
 
 }
