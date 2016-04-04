e7b290c55f25754717b19df51b2d4f05785ee1b8
ncannasse <ncannasse@gmail.com>
2016-04-04 17:57:23 +0200

    use a cache for small ogg data (prevent slow decoding issues)

--- hxd/snd/OggData.hx
+++ hxd/snd/OggData.hx
@@ -1,40 +1,88 @@
 package hxd.snd;
 
 private class BytesOutput extends haxe.io.Output {
-	public var bytes : haxe.io.Bytes;
-	public var position : Int;
+
+	var bytes : haxe.io.Bytes;
+	var position : Int;
+	#if flash
+	var m : hxd.impl.Memory.MemoryReader;
+	#end
+
 	public function new() {
 	}
-	#if flash
+
+	public function done() {
+		#if flash
+		m.end();
+		#end
+	}
+
+	public function init( bytes : haxe.io.Bytes, position : Int ) {
+		this.bytes = bytes;
+		this.position = position;
+		#if flash
+		m = hxd.impl.Memory.select(bytes);
+		#end
+	}
+
 	override function writeFloat(f) {
+		#if flash
+		m.wfloat(position, f);
+		#else
 		bytes.setFloat(position, f);
+		#end
 		position += 4;
 	}
-	#else
-	override function writeInt32(v) {
-		bytes.setInt32(position, v);
-		position += 4;
-	}
-	#end
+
 }
 
 class OggData extends Data {
 
 	var reader : stb.format.vorbis.Reader;
 	var output : BytesOutput;
+	var decodedFirst : Int;
+	var decodedLast : Int;
+	var decoded : haxe.io.Bytes;
+	static inline var CACHED_SAMPLES = 44100 * 3; // 3s of cached sound
 
 	public function new( bytes : haxe.io.Bytes ) {
 		reader = stb.format.vorbis.Reader.openFromBytes(bytes);
 		samples = reader.totalSample;
 		output = new BytesOutput();
-		trace(samples);
+		decodedFirst = 0;
+		decodedLast = 0;
 	}
 
 	override public function decode(out:haxe.io.Bytes, outPos:Int, sampleStart:Int, sampleCount:Int) {
-		reader.currentSample = sampleStart;
-		output.bytes = out;
-		output.position = outPos;
-		reader.read(output, sampleCount, 2, 44100, true);
+		var last = sampleStart + sampleCount;
+		if( last > samples )
+			throw "OUTSIDE";
+		if( sampleStart < decodedFirst || last > decodedLast ) {
+			var need = sampleCount - sampleStart;
+			if( need > CACHED_SAMPLES || samples > CACHED_SAMPLES ) {
+				// directly decode in out
+				output.init(out, outPos);
+				reader.currentSample = sampleStart;
+				reader.read(output, sampleCount, 2, 44100, true);
+				output.done();
+				return;
+			}
+			if( decoded == null )
+				decoded = haxe.io.Bytes.alloc((samples < CACHED_SAMPLES ? samples : CACHED_SAMPLES) * 2 * 4);
+			need = CACHED_SAMPLES;
+			if( sampleStart + need > samples ) need = samples - sampleStart;
+			output.init(decoded,0);
+			reader.currentSample = sampleStart;
+			reader.read(output, need, 2, 44100, true);
+			output.done();
+			decodedFirst = sampleStart;
+			decodedLast = sampleStart + need;
+			if( sampleStart == 0 && need == samples ) {
+				reader = null;
+				output = null;
+			}
+		}
+		out.blit(outPos, decoded, (sampleStart - decodedFirst) * 2 * 4, sampleCount * 2 * 4);
 	}
 
 
