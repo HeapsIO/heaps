26845860ac12f19f547d03e33cb15c2de139c809
ncannasse <ncannasse@gmail.com>
2016-04-13 15:33:26 +0200

    fixed API change (close #167)

--- h2d/filter/DropShadow.hx
+++ h2d/filter/DropShadow.hx
@@ -5,11 +5,13 @@ class DropShadow extends Glow {
 
 	public var distance : Float;
 	public var angle : Float;
+	var alphaPass = new h3d.mat.Pass("");
 
 	public function new( distance : Float = 4., angle : Float = 0.785, color : Int = 0, alpha = 1., quality = 1, passes = 1, sigma = 1. ) {
 		super(color, alpha, quality, passes, sigma);
 		this.distance = distance;
 		this.angle = angle;
+		alphaPass.addShader(new h3d.shader.UVDelta());
 	}
 
 	override function sync(ctx, s) {
@@ -25,7 +27,8 @@ class DropShadow extends Glow {
 		pass.apply(save, ctx.textures.allocTarget("glowTmp", ctx, t.width, t.height, false));
 		var dx = Math.round(Math.cos(angle) * distance);
 		var dy = Math.round(Math.sin(angle) * distance);
-		h3d.pass.Copy.run(t.getTexture(), save, Alpha, new h3d.Vector( dx / t.width, dy / t.height) );
+		alphaPass.getShader(h3d.shader.UVDelta).uvDelta.set(dx / t.width, dy / t.height);
+		h3d.pass.Copy.run(t.getTexture(), save, Alpha, alphaPass );
 		var ret = h2d.Tile.fromTexture(save);
 		ret.dx = dx;
 		ret.dy = dy;
