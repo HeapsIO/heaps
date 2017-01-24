a938602d259ef72e838d7ec7386dfd583fb1c70d
Pascal Peridont <pperidont@motion-twin.com>
2017-01-24 16:06:01 +0100

    reset boundTextures in setRenderTarget

--- h3d/impl/GlDriver.hx
+++ h3d/impl/GlDriver.hx
@@ -972,6 +972,8 @@ class GlDriver extends Driver {
 		else
 			gl.framebufferRenderbuffer(GL.FRAMEBUFFER, GL.DEPTH_ATTACHMENT, GL.RENDERBUFFER, null);
 		gl.viewport(0, 0, tex.width >> mipLevel, tex.height >> mipLevel);
+		for( i in 0...boundTextures.length )
+			boundTextures[i] = null;
 	}
 
 	override function setRenderTargets( textures : Array<h3d.mat.Texture> ) {
