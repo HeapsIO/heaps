a7a6525b13f2d214173627df4c656fcdb37df5b2
Nicolas Cannasse <ncannasse@gmail.com>
2017-02-19 22:07:02 +0100

    minor

--- h3d/impl/GlDriver.hx
+++ h3d/impl/GlDriver.hx
@@ -189,7 +189,6 @@ class GlDriver extends Driver {
 		gl.shaderSource(s, code);
 		gl.compileShader(s);
 		var log = gl.getShaderInfoLog(s);
-		if( log != "" ) trace(log);
 		if ( gl.getShaderParameter(s, GL.COMPILE_STATUS) != cast 1 ) {
 			var log = gl.getShaderInfoLog(s);
 			var lid = Std.parseInt(log.substr(9));
