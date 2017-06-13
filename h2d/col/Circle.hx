fb2075588276ceaaff1f3c16a829e73f6be6fd62
bstouls <vallandre@hotmail.com>
2017-06-13 17:59:49 +0200

    add lineIntersects

--- h2d/col/Circle.hx
+++ h2d/col/Circle.hx
@@ -44,6 +44,26 @@ class Circle {
 		return true;
 	}
 
+	public inline function lineIntersect(p1 : h2d.col.Point, p2:h2d.col.Point) {
+		var dx = p2.x - p1.x;
+		var dy = p2.y - p1.y;
+		var a = dx * dx + dy * dy;
+		if (a < 1e-8) return null;
+		var b = 2 * (dx * (p1.x - x) + dy * (p1.y - y));
+		var c = hxd.Math.distanceSq(p1.x - x, p1.y - y) - ray * ray;
+		var d = b * b - 4 * a * c;
+
+		if(d < 0) return null;
+		if(d == 0) {
+			var t = -b / (2 * a);
+			return [new h2d.col.Point(p1.x + t * dx, p1.y + t * dy)];
+		}
+
+		var t1 = (-b - Math.sqrt(d)) / (2 * a);
+		var t2 = (-b + Math.sqrt(d)) / (2 * a);
+		return [new h2d.col.Point(p1.x + t1 * dx, p1.y + t1 * dy), new h2d.col.Point(p1.x + t2 * dx, p1.y + t2 * dy)];
+	}
+
 	public function toString() {
 		return '{${Math.fmt(x)},${Math.fmt(y)},${Math.fmt(ray)}}';
 	}
