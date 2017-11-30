b24f4c2675a15454cb5c48701481387bef7f498d
trethaller <tom.rethaller@gmail.com>
2017-11-30 10:59:24 +0100

    Add Grid.addUVs()

--- h3d/prim/Grid.hx
+++ h3d/prim/Grid.hx
@@ -2,7 +2,13 @@ package h3d.prim;
 
 class Grid extends Polygon {
 
+	var width : Int;
+	var height : Int;
+
 	public function new( width : Int, height : Int, cellWidth = 1., cellHeight = 1. ) {
+		this.width = width;
+		this.height = height;
+
 		var idx = new hxd.IndexBuffer();
 		for( y in 0...height )
 			for( x in 0...width ) {
@@ -18,6 +24,22 @@ class Grid extends Polygon {
 			[for( y in 0...height + 1 ) for( x in 0...width + 1 ) new h3d.col.Point(x * cellWidth, y * cellHeight, 0)],
 			idx
 		);
+	}
+
+	override function addUVs() {
+		unindex();
+		uvs = [];
+		for( y in 0...height ) {
+			for( x in 0...width ) {
+				uvs.push(new UV(x/width,     y/height));
+				uvs.push(new UV((x+1)/width, y/height));
+				uvs.push(new UV(x/width,     (y+1)/height));
+
+				uvs.push(new UV((x+1)/width, y/height));
+				uvs.push(new UV((x+1)/width, (y+1)/height));
+				uvs.push(new UV(x/width,     (y+1)/height));				
+			}
+		}
 	}
 
 }
