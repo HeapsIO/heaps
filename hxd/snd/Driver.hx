21c92f3e8d071a9e4b44fa5529be4f53b6cd2dd7
ncannasse <ncannasse@gmail.com>
2018-01-26 13:06:50 +0100

    added

---
+++ hxd/snd/Driver.hx
@@ -0,0 +1,52 @@
+package hxd.snd;
+
+#if usesys
+typedef SourceHandle = haxe.AudioTypes.SourceHandle;
+typedef BufferHandle = haxe.AudioTypes.BufferHandle;
+#else
+typedef SourceHandle = hxd.snd.openal.AudioTypes.SourceHandle;
+typedef BufferHandle = hxd.snd.openal.AudioTypes.BufferHandle;
+#end
+
+enum SourceState {
+	Stopped;
+	Playing;
+	Unhandled;
+}
+
+class EffectDriver<T> {
+	public function new() {}
+
+	public function acquire () : Void {};
+	public function release () : Void {};
+	public function update  (e : T) : Void {};
+	public function bind    (e : T, source : SourceHandle) : Void {};
+	public function apply   (e : T, source : SourceHandle) : Void {};
+	public function unbind  (e : T, source : SourceHandle) : Void {};
+}
+
+interface Driver {
+	public function setMasterVolume     (value : Float) : Void;
+	public function setListenerParams   (position : h3d.Vector, direction : h3d.Vector, up : h3d.Vector, ?velocity : h3d.Vector) : Void;
+
+	public function createSource         () : SourceHandle;
+	public function playSource           (source : SourceHandle) : Void;
+	public function stopSource           (source : SourceHandle) : Void;
+	public function getSourceState       (source : SourceHandle) : SourceState;
+	public function setSourceVolume      (source : SourceHandle, value : Float) : Void;
+	public function destroySource        (source : SourceHandle) : Void; 
+
+	public function createBuffer         () : BufferHandle;
+	public function setBufferData        (buffer : BufferHandle, data : haxe.io.Bytes, size : Int, format : Data.SampleFormat, channelCount : Int, samplingRate : Int) : Void;
+	public function destroyBuffer        (buffer : BufferHandle) : Void;
+
+	public function queueBuffer          (source : SourceHandle, buffer : BufferHandle, sampleStart : Int, endOfStream : Bool) : Void;
+	public function unqueueBuffer        (source : SourceHandle, buffer : BufferHandle) : Void;
+	public function getProcessedBuffers  (source : SourceHandle) : Int;
+	public function getPlayedSampleCount (source : SourceHandle) : Int;
+
+	public function update  () : Void;
+	public function dispose () : Void;
+
+	public function getEffectDriver(type : String) : EffectDriver<Dynamic>;
+}
