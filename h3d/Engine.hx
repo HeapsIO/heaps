c2d1c59a4e9368737ae2f041f6c7359894b49ae3
Nicolas Cannasse <ncannasse@gmail.com>
2018-04-28 15:22:01 +0200

    more explicit error message when no output driver

--- h3d/Engine.hx
+++ h3d/Engine.hx
@@ -72,7 +72,7 @@ class Engine {
 		#elseif usesys
 		driver = new haxe.GraphicsDriver(antiAlias);
 		#else
-		#if sys Sys.println #else trace #end("No output driver available.");
+		#if sys Sys.println #else trace #end("No output driver available." #if hl + " Compile with -lib hlsdl or -lib hldx" #end);
 		driver = new h3d.impl.LogDriver(new h3d.impl.NullDriver());
 		driver.logEnable = true;
 		#end
