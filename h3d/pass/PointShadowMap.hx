31e76c50ccb021d757bdc023940f6f0b800e26b3
ShiroSmith <41334615+ShiroSmith@users.noreply.github.com>
2018-08-17 10:36:55 +0200

    - Undo temp fix

--- h3d/pass/PointShadowMap.hx
+++ h3d/pass/PointShadowMap.hx
@@ -84,7 +84,7 @@ class PointShadowMap extends Shadows {
 
 	override function loadStaticData( bytes : haxe.io.Bytes ) {
 		if( (mode != Mixed && mode != Static) || bytes == null )
-			return true;
+			return false;
 		var buffer = new haxe.io.BytesInput(bytes);
 		var size = buffer.readInt32();
 		if( size != this.size )
