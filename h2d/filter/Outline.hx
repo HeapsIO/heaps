d8eaa95edb495f2d0d3f44b7827ef63ef0925c47
Leo Bergman <leo@hacksawstudios.com>
2018-09-03 09:59:31 +0200

    Add multiplyAlpha.

--- h2d/filter/Outline.hx
+++ h2d/filter/Outline.hx
@@ -4,27 +4,35 @@ class Outline extends Filter {
 	public var size(default, set) : Int;
 	public var color(default, set) : Int;
 	public var quality(default, set) : Float;
+	public var multiplyAlpha(default, set) : Bool;
 
-	var pass:h3d.pass.Outline;
+	var pass : h3d.pass.Outline;
 
-	public function new( size = 4, color = 0xFF000000, quality = 0.3 ) {
+	public function new(size = 4, color = 0xFF000000, quality = 0.3, multiplyAlpha = true) {
 		super();
 		smooth = true;
-		pass = new h3d.pass.Outline(size, color, quality);
+		pass = new h3d.pass.Outline(size, color, quality, multiplyAlpha);
 	}
 
 	inline function get_size() return pass.size;
+
 	inline function set_size(v) return pass.size = v;
+
 	inline function get_color() return pass.color;
+
 	inline function set_color(v) return pass.color = v;
+
 	inline function get_quality() return pass.quality;
+
 	inline function set_quality(v) return pass.quality = v;
 
-	override function sync( ctx : RenderContext, s : Sprite ) {
+	inline function set_multiplyAlpha(v) return pass.multiplyAlpha = v;
+
+	override function sync(ctx : RenderContext, s : Sprite) {
 		boundsExtend = pass.size * 2;
 	}
 
-	override function draw( ctx : RenderContext, t : h2d.Tile ) {
+	override function draw(ctx : RenderContext, t : h2d.Tile) {
 		var out = t.getTexture();
 		var old = out.filter;
 		out.filter = Linear;
