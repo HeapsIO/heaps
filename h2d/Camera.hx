5ceb4d567645449d87ed3fb637fdab724f27c35e
Yanrishatum <krabanek@gmail.com>
2018-10-27 07:36:15 +0300

    Add scene checks Allow creating camera without adding it. Allow removing camera from scene.

--- h2d/Camera.hx
+++ h2d/Camera.hx
@@ -20,13 +20,18 @@ class Camera extends h2d.Object {
 	private var camX : Float;
 	private var camY : Float;
 
-	public function new(scene : h2d.Scene) {
+	public function new(?scene : h2d.Scene) {
 		super(scene);
 		this.scene = scene;
-		this.width = scene.width;
-		this.height = scene.height;
-		this.halfWidth = width * 0.5;
-		this.halfHeight = height * 0.5;
+		if ( scene != null ) {
+			this.width = scene.width;
+			this.height = scene.height;
+			this.halfWidth = width * 0.5;
+			this.halfHeight = height * 0.5;
+		} else {
+			this.width = 0; this.height = 0;
+			this.halfWidth = 0; this.halfHeight = 0;
+		}
 	}
 
 	inline function get_centerX() { return this.x + halfWidth; }
@@ -41,27 +46,60 @@ class Camera extends h2d.Object {
 		return v;
 	}
 
+	override private function onAdd()
+	{
+		if ( !Std.is(parent, h2d.Scene) ) throw "Camera can be added only to h2d.Scene!";
+		this.scene = cast parent;
+		super.onAdd();
+	}
+
+	override private function onRemove()
+	{
+		this.scene = null;
+		super.onRemove();
+	}
+
 	override private function calcAbsPos()
 	{
-		if( rotation == 0 ) {
-			camA = scaleX * scene.matA;
-			camB = scaleX * scene.matB;
-			camC = scaleY * scene.matC;
-			camD = scaleY * scene.matD;
+		if (scene == null) {
+			var cr, sr;
+			if( rotation == 0 ) {
+				cr = 1.; sr = 0.;
+				camA = scaleX;
+				camB = 0;
+				camC = 0;
+				camD = scaleY;
+			} else {
+				cr = Math.cos(rotation);
+				sr = Math.sin(rotation);
+				camA = scaleX * cr;
+				camB = scaleX * sr;
+				camC = scaleY * -sr;
+				camD = scaleY * cr;
+			}
+			camX = x;
+			camY = y;
 		} else {
-			var cr = Math.cos(rotation);
-			var sr = Math.sin(rotation);
-			var tmpA = scaleX * cr;
-			var tmpB = scaleX * sr;
-			var tmpC = scaleY * -sr;
-			var tmpD = scaleY * cr;
-			camA = tmpA * scene.matA + tmpB * scene.matC;
-			camB = tmpA * scene.matB + tmpB * scene.matD;
-			camC = tmpC * scene.matA + tmpD * scene.matC;
-			camD = tmpC * scene.matB + tmpD * scene.matD;
+			if( rotation == 0 ) {
+				camA = scaleX * scene.matA;
+				camB = scaleX * scene.matB;
+				camC = scaleY * scene.matC;
+				camD = scaleY * scene.matD;
+			} else {
+				var cr = Math.cos(rotation);
+				var sr = Math.sin(rotation);
+				var tmpA = scaleX * cr;
+				var tmpB = scaleX * sr;
+				var tmpC = scaleY * -sr;
+				var tmpD = scaleY * cr;
+				camA = tmpA * scene.matA + tmpB * scene.matC;
+				camB = tmpA * scene.matB + tmpB * scene.matD;
+				camC = tmpC * scene.matA + tmpD * scene.matC;
+				camD = tmpC * scene.matB + tmpD * scene.matD;
+			}
+			camX = -x * scene.matA + -y * scene.matC + scene.absX;
+			camY = -x * scene.matB + -y * scene.matD + scene.absY;
 		}
-		camX = -x * scene.matA + -y * scene.matC + scene.absX;
-		camY = -x * scene.matB + -y * scene.matD + scene.absY;
 	}
 
 	override private function sync(ctx : RenderContext)
