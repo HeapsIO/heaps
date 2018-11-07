4b49e270f54fe393e75a05f828ff758a7ea7c308
ShiroSmith <41334615+ShiroSmith@users.noreply.github.com>
2018-11-07 15:24:59 +0100

    Add missing file

---
+++ h3d/shader/Emissive.hx
@@ -0,0 +1,20 @@
+package h3d.shader;
+
+class Emissive extends hxsl.Shader {
+
+	static var SRC = {
+		var pixelColor : Vec4;
+		@param var emissive : Float;
+
+		function fragment() {
+			pixelColor.rgb += pixelColor.rgb * emissive * pixelColor.a;
+		}
+
+	};
+
+	public function new( emissive : Float = 0 ) {
+		super();
+		this.emissive = emissive;
+	}
+
+}
