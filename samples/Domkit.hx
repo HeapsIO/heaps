8cd7871c1bd4eb7d8dc521f923dece6867563e5d
ncannasse <ncannasse@gmail.com>
2019-02-20 14:18:54 +0100

    fixed domkit sample

--- samples/Domkit.hx
+++ samples/Domkit.hx
@@ -4,13 +4,13 @@
 class ViewComp extends h2d.Flow implements h2d.domkit.Object {
 
 	static var SRC =
-	<flow class="mybox" min-width="200" content-halign={align}>
+	<view class="mybox" min-width="200" content-halign={align}>
 		Hello World
 		${
 			for( i in icons )
 				<bitmap src={i} id="icons[]"/>
 		}
-	</flow>;
+	</view>;
 
 	public function new(align:h2d.Flow.FlowAlign,icons:Array<h2d.Tile>,?parent) {
 		super(parent);
