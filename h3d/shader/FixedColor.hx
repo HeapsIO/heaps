e87b671b9f134dd8f03cab13f839877bb3a70b5c
ShiroSmith <41334615+ShiroSmith@users.noreply.github.com>
2019-07-12 12:20:43 +0200

    Update FixedColor shader

--- h3d/shader/FixedColor.hx
+++ h3d/shader/FixedColor.hx
@@ -4,9 +4,11 @@ class FixedColor extends hxsl.Shader {
 
 	static var SRC = {
 		@param var color : Vec4;
+		@const @param var USE_ALPHA : Bool;
 		var output : { color : Vec4 };
 		function fragment() {
-			output.color = color;
+			output.color.rgb = color.rgb;
+			if( USE_ALPHA ) output.color.a = color.a;
 		}
 	}
 
@@ -14,6 +16,7 @@ class FixedColor extends hxsl.Shader {
 		super();
 		this.color.setColor(color);
 		this.color.w = alpha;
+		USE_ALPHA = true;
 	}
 
 }
