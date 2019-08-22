d5b60f10b97eb2e37c2ecb79dd203e52822dc79d
Rasmus Brinck <x@rasmusbrinck.se>
2019-08-22 14:49:22 +0200

    chore: more formatting fix

--- h2d/Scene.hx
+++ h2d/Scene.hx
@@ -644,8 +644,7 @@ class Scene extends Layers implements h3d.IDrawable implements hxd.SceneEvents.I
 	}
 
 	function drawImplTo( s : Object, t : h3d.mat.Texture ) {
-		if( !t.flags.has(Target) )
-			throw "Can only draw to texture created with Target flag";
+		if( !t.flags.has(Target) ) throw "Can only draw to texture created with Target flag";
 		var needClear = !t.flags.has(WasCleared);
 		ctx.engine = h3d.Engine.getCurrent();
 		ctx.engine.begin(true);
