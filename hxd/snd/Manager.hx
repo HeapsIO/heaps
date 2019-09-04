4700e362b2da8bfd575026ec10f6a175eb6408a0
trethaller <tom.rethaller@gmail.com>
2019-09-04 14:10:07 +0200

    sound channels: prevent negative volumes

--- hxd/snd/Manager.hx
+++ hxd/snd/Manager.hx
@@ -452,6 +452,7 @@ class Manager {
 			var v = c.currentVolume * volume;
 			if (s.volume != v) {
 				s.volume = v;
+				if (s.volume < 0) s.volume = 0;
 				driver.setSourceVolume(s.handle, v);
 				#if hlopenal
 				if( v > 1 ) Sys.println("Could not set volume " + v + " on " + c.sound);
