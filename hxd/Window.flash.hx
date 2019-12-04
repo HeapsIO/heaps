27587b2826d89f3c9ec79cd846d2b44f498f448b
jefvel <akselkornesjo@gmail.com>
2019-12-04 20:32:13 +0100

    Flash displayMode works

--- hxd/Window.flash.hx
+++ hxd/Window.flash.hx
@@ -20,6 +20,7 @@ class Window {
 	public var isFocused(get, never) : Bool;
 
 	public var title(get, set) : String;
+	@:isVar
 	public var displayMode(get, set) : DisplayMode;
 
 	// FLASH
@@ -117,18 +118,7 @@ class Window {
 
 	@:deprecated("Use the displayMode property instead")
 	public function setFullScreen( v : Bool ) : Void {
-		var isAir = isAir();
-		var state = v ? (isAir ? flash.display.StageDisplayState.FULL_SCREEN_INTERACTIVE : flash.display.StageDisplayState.FULL_SCREEN) : flash.display.StageDisplayState.NORMAL;
-		if( stage.displayState != state ) {
-			var t = flash.Lib.getTimer();
-			// delay first fullsrceen toggle on OSX/Air to prevent the command window to spawn over
-			if( v && isAir && t < 5000 && !fsDelayed && flash.system.Capabilities.os.indexOf("Mac") != -1 ) {
-				fsDelayed = true;
-				haxe.Timer.delay(function() this.setFullScreen(v), 1000);
-				return;
-			}
-			stage.displayState = state;
-		}
+		this.displayMode = v ? Borderless : Windowed;
 	}
 
 	static var inst : Window = null;
@@ -306,7 +296,7 @@ class Window {
 	function get_isFocused() : Bool return false;
 
 	function get_displayMode() : DisplayMode {
-		if (stage.displayMode == flash.display.StageDisplayState.NORMAL) {
+		if (stage.displayState == flash.display.StageDisplayState.NORMAL) {
 			return Windowed;
 		}
 
@@ -322,21 +312,22 @@ class Window {
 			// delay first fullsrceen toggle on OSX/Air to prevent the command window to spawn over
 			if( fullscreen && isAir && t < 5000 && !fsDelayed && flash.system.Capabilities.os.indexOf("Mac") != -1 ) {
 				fsDelayed = true;
-				haxe.Timer.delay(function() this.displayMode = m, 1000);
-				return;
+				haxe.Timer.delay(function() { this.displayMode = m; }, 1000);
+				return m;
 			}
+			trace(m);
 			stage.displayState = state;
 		}
+
+		return m;
 	}
 
 	function get_title() : String {
-		// !TODO implement
 		return "";
 	}
 
 	function set_title( t : String ) : String {
-		// !TODO implement
-		return r;
+		return t;
 	}
 }
 
