37f7d62533e390dc67bcd4fc0c3808c1c1687fc6
Yanrishatum <krabanek@gmail.com>
2019-12-08 19:26:40 +0300

    WIP: Heaps CLI

---
+++ tools/project/src/Main.hx
@@ -0,0 +1,21 @@
+
+
+class Main extends hxd.App {
+
+	static function main() {
+		new Main();
+	}
+
+	override function loadAssets(onLoaded:() -> Void) {
+		// Call hxd.Res.init here
+		// hxd.Res.initEmbed();
+		hxd.Res.initLocal();
+		onLoaded();
+	}
+
+	override function init() {
+		// Entry point
+		
+	}
+
+}
