0e6743f1cd3361b97d29f70e67bc4a36955a30ba
Kryvosheya Sergij <syowork@gmail.com>
2020-01-12 18:17:16 +0200

    Added angle() and length() methods. Cache for dx, dy

--- h2d/col/Line.hx
+++ h2d/col/Line.hx
@@ -5,18 +5,21 @@ class Line {
 	public var p1 : Point;
 	public var p2 : Point;
 
+	private var dx:Float;
+	private var dy:Float;
+
 	public inline function new(p1,p2) {
 		this.p1 = p1;
 		this.p2 = p2;
+		this.dx = p2.x - p1.x;
+		this.dy = p2.y - p1.y;
 	}
 
 	public inline function side( p : Point ) {
-		return (p2.x - p1.x) * (p.y - p1.y) - (p2.y - p1.y) * (p.x - p1.x);
+		return dx * (p.y - p1.y) - dy * (p.x - p1.x);
 	}
 
 	public inline function project( p : Point ) {
-		var dx = p2.x - p1.x;
-		var dy = p2.y - p1.y;
 		var k = ((p.x - p1.x) * dx + (p.y - p1.y) * dy) / (dx * dx + dy * dy);
 		return new Point(dx * k + p1.x, dy * k + p1.y);
 	}
@@ -42,8 +45,6 @@ class Line {
 	}
 
 	public inline function distanceSq( p : Point ) {
-		var dx = p2.x - p1.x;
-		var dy = p2.y - p1.y;
 		var k = ((p.x - p1.x) * dx + (p.y - p1.y) * dy) / (dx * dx + dy * dy);
 		var mx = dx * k + p1.x - p.x;
 		var my = dy * k + p1.y - p.y;
@@ -52,6 +53,14 @@ class Line {
 
 	public inline function distance( p : Point ) {
 		return hxd.Math.sqrt(distanceSq(p));
+	}
+
+	public inline function angle() {
+		return Math.atan2(dy, dx);
+	}
+
+	public inline function lenght():Float {
+		return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
 	}
 
 }
