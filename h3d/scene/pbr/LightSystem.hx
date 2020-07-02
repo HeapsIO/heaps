0b21c83f1ba7161726c5d34da57c8bed0b5eafb0
Nicolas Cannasse <ncannassse@gmail.com>
2020-07-02 13:13:04 +0200

    added Shadows Step, rename pbr.LightSystem.drawLight to drawShadows

--- h3d/scene/pbr/LightSystem.hx
+++ h3d/scene/pbr/LightSystem.hx
@@ -14,7 +14,7 @@ class LightSystem extends h3d.scene.LightSystem {
 	}
 
 
-	public function drawLight( light : Light, passes : h3d.pass.PassList ) {
+	public function drawShadows( light : Light, passes : h3d.pass.PassList ) {
 		light.shadows.setContext(ctx);
 		light.shadows.draw(passes);
 		passes.reset();
