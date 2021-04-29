284a957e35a50d06246d0f606ab541c5eb9fde13
Nicolas Cannasse <ncannasse@gmail.com>
2021-04-29 12:48:07 +0200

    fix

--- h3d/scene/pbr/Renderer.hx
+++ h3d/scene/pbr/Renderer.hx
@@ -132,7 +132,7 @@ class Renderer extends h3d.scene.Renderer {
 
 	override function getPassByName(name:String):h3d.pass.Base {
 		switch( name ) {
-		case "overlay", "beforeTonemapping", "albedo", "afterTonemapping", "forward":
+		case "overlay", "beforeTonemapping", "albedo", "afterTonemapping", "forward", "forwardAlpha":
 			return defaultPass;
 		case "default", "alpha", "additive":
 			return output;
