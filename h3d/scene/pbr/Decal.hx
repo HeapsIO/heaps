8c15fb81e052fe260b580c0cbe84761391e7d5be
trethaller <tom.rethaller@gmail.com>
2021-09-09 12:10:10 +0200

    Decal: don't update shaders during sync even when culled

--- h3d/scene/pbr/Decal.hx
+++ h3d/scene/pbr/Decal.hx
@@ -6,20 +6,16 @@ class Decal extends Mesh {
 		super(primitive, material, parent);
 	}
 
-	override function sync( ctx : RenderContext ) {
-		super.sync(ctx);
+	override function emit( ctx : RenderContext ) {
+		super.emit(ctx);
 
-		var shader = material.mainPass.getShader(h3d.shader.pbr.VolumeDecal.DecalPBR);
-		if( shader != null )
-			syncDecalPBR(shader);
+		if(ctx.visibleFlag) {
+			var shader = material.mainPass.getShader(h3d.shader.pbr.VolumeDecal.DecalPBR);
+			if( shader != null ) {
+				var absPos = getAbsPos();
+				shader.normal.set(absPos._31, absPos._32, absPos._33); // up
+				shader.tangent.set(absPos._21, absPos._22, absPos._23); // right
+			}
+		}
 	}
-
-	function syncDecalPBR( shader : h3d.shader.pbr.VolumeDecal.DecalPBR ) {
-		var absPos = getAbsPos();
-		shader.normal.set(absPos._31, absPos._32, absPos._33); // up
-		shader.tangent.set(absPos._21, absPos._22, absPos._23); // right
-	}
-
-
-
 }
