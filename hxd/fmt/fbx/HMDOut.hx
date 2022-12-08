fe3adb8d80628f9277f72e8a38ec255ebac90ec1
clementlandrin <clement.landrin@hotmail.fr>
2022-12-08 10:39:05 +0100

    Error message building tangents without uvs.

--- hxd/fmt/fbx/HMDOut.hx
+++ hxd/fmt/fbx/HMDOut.hx
@@ -37,6 +37,9 @@ class HMDOut extends BaseLibrary {
 		var uvs = geom.getUVs();
 		var index = geom.getIndexes();
 
+		if ( index.vidx.length > 0 && uvs[0] == null )
+			throw "Need UVs to build tangents";
+
 		#if (hl && !hl_disable_mikkt && (haxe_ver >= "4.0"))
 		var m = new hl.Format.Mikktspace();
 		m.buffer = new hl.Bytes(8 * 4 * index.vidx.length);
