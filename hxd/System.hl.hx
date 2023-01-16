4c3c4eeccc7959bdb2ec33f22141bbcc2737f9d4
Ryan Cleven <rcleven@mac.com>
2023-01-16 09:06:11 -0800

    Compile define -D heaps_no_error_trap to suppress trapping exceptions… (#1121)

--- hxd/System.hl.hx
+++ hxd/System.hl.hx
@@ -139,9 +139,10 @@ class System {
 		var eventRecycle = [];
 		#end
 		while( true ) {
+			#if !heaps_no_error_trap
 			try {
 				hl.Api.setErrorHandler(reportError); // set exception trap
-
+			#end
 				#if ( target.threaded && (haxe_ver >= 4.2) )
 				// Due to how 4.2+ timers work, instead of MainLoop, thread events have to be updated.
 				// Unsafe events rely on internal implementation of EventLoop, but utilize the recycling feature
@@ -156,9 +157,11 @@ class System {
 				#end
 
 				if( !mainLoop() ) break;
+			#if !heaps_no_error_trap
 			} catch( e : Dynamic ) {
 				hl.Api.setErrorHandler(null);
 			}
+			#end
 			#if hot_reload
 			if( check_reload() ) onReload();
 			#end
