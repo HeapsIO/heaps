2a0f55f6584311a0fece0e4aebbaacfb55d973cd
lviguier <leo.vgr@hotmail.com>
2024-01-02 09:41:41 +0100

    fbx exporter: building globale settings node

--- hxd/fmt/fbx/Writer.hx
+++ hxd/fmt/fbx/Writer.hx
@@ -155,6 +155,36 @@ class Writer {
 		return header;
 	}
 
+	function buildProperties() {
+		var properties : FbxNode = { name:"Properties70", props: null, childs: [
+			{ name: "P", props: [PString("UpAxis"), PString("int"), PString("Integer"), PString(""), PInt(2) ], childs:null }, // Z up
+			{ name: "P", props: [PString("UpAxisSign"), PString("int"), PString("Integer"), PString(""), PInt(1) ], childs:null },
+			{ name: "P", props: [PString("FrontAxis"), PString("int"), PString("Integer"), PString(""), PInt(0) ], childs:null }, // X front
+			{ name: "P", props: [PString("FrontAxisSign"), PString("int"), PString("Integer"), PString(""), PInt(1) ], childs:null },
+			{ name: "P", props: [PString("CoordAxis"), PString("int"), PString("Integer"), PString(""), PInt(1) ], childs:null }, // Y coord
+			{ name: "P", props: [PString("CoordAxisSign"), PString("int"), PString("Integer"), PString(""), PInt(1) ], childs:null },
+			{ name: "P", props: [PString("OriginalUpAxis"), PString("int"), PString("Integer"), PString(""), PInt(-1) ], childs:null },
+			{ name: "P", props: [PString("OriginalUpAxisSign"), PString("int"), PString("Integer"), PString(""), PInt(1) ], childs:null },
+			{ name: "P", props: [PString("UnitScaleFactor"), PString("double"), PString("Number"), PString(""), PInt(1) ], childs:null },
+			{ name: "P", props: [PString("OriginalUnitScaleFactor"), PString("double"), PString("Number"), PString(""), PInt(1) ], childs:null },
+			{ name: "P", props: [PString("AmbientColor"), PString("ColorRGB"), PString("Color"), PString(""), PInt(0), PInt(0), PInt(0) ], childs:null },
+			{ name: "P", props: [PString("DefaultCamera"), PString("KString"), PString(""), PString(""), PString("Producer Perspective") ], childs:null },
+			{ name: "P", props: [PString("TimeMode"), PString("enum"), PString(""), PString(""), PInt(11) ], childs:null },
+			{ name: "P", props: [PString("TimeSpanStart"), PString("Ktime"), PString("Time"), PString(""), PInt(0) ], childs:null },
+			{ name: "P", props: [PString("TimeSpanStop"), PString("Ktime"), PString("Time"), PString(""), PInt(0) ], childs:null },
+			{ name: "P", props: [PString("CustomFrameRate"), PString("double"), PString("Number"), PString(""), PInt(24) ], childs:null },
+		] };
+		return properties;
+	}
+
+	function buildGlobalSettings() {
+		var version : FbxNode = {name:"Version", props: [PInt(1000)], childs: null};
+		var properties : FbxNode = buildProperties();
+
+		var globalSettings : FbxNode = {name:"GlobalSettings", props: null, childs: [version, properties]};
+		return globalSettings;
+	}
+
 	public function write(fbx:FbxNode) {
 		var old = out;
 		var header = new haxe.io.BytesOutput();
@@ -164,6 +194,7 @@ class Writer {
 		// if( version > Data.CURRENT_VERSION ) throw "Can't write HMD v" + version;
 
 		writeHeader();
+		writeNode(buildGlobalSettings());
 
 		// // We dont't want the root node to be written here
 		// for (c in fbx.childs) {
@@ -172,100 +203,10 @@ class Writer {
 
 		// writeNode(node);
 
-		// out.writeInt32(d.geometries.length);
-		// for( g in d.geometries ) {
-		// 	writeProps(g.props);
-		// 	out.writeInt32(g.vertexCount);
-		// 	out.writeByte(g.vertexFormat.stride);
-		// 	out.writeByte(@:privateAccess g.vertexFormat.inputs.length);
-		// 	for( f in g.vertexFormat.getInputs() ) {
-		// 		writeName(f.name);
-		// 		out.writeByte(f.type.toInt() | (f.precision.toInt() << 4));
-		// 	}
-		// 	out.writeInt32(g.vertexPosition);
-		// 	if( g.indexCounts.length >= 0xFF ) {
-		// 		out.writeByte(0xFF);
-		// 		out.writeInt32(g.indexCounts.length);
-		// 	} else
-		// 		out.writeByte(g.indexCounts.length);
-		// 	for( i in g.indexCounts )
-		// 		out.writeInt32(i);
-		// 	out.writeInt32(g.indexPosition);
-		// 	writeBounds(g.bounds);
-		// }
-
-		// out.writeInt32(d.materials.length);
-		// for( m in d.materials ) {
-		// 	writeProps(m.props);
-		// 	writeName(m.name);
-		// 	writeName(m.diffuseTexture);
-		// 	out.writeByte(m.blendMode.getIndex());
-		// 	out.writeByte(1); // old culling back
-		// 	writeFloat(1); // old killalpha
-		// 	if( m.props != null && m.props.indexOf(HasExtraTextures) >= 0 ) {
-		// 		writeName(m.specularTexture);
-		// 		writeName(m.normalMap);
-		// 	}
-		// }
-
-		// out.writeInt32(d.models.length);
-		// for( m in d.models ) {
-		// 	writeProps(m.props);
-		// 	writeName(m.name);
-		// 	out.writeInt32(m.parent + 1);
-		// 	writeName(m.follow);
-		// 	writePosition(m.position);
-		// 	out.writeInt32(m.geometry + 1);
-		// 	if( m.geometry < 0 ) continue;
-		// 	if( m.materials.length >= 0xFF ) {
-		// 		out.writeByte(0xFF);
-		// 		out.writeInt32(m.materials.length);
-		// 	} else
-		// 		out.writeByte(m.materials.length);
-		// 	for( m in m.materials )
-		// 		out.writeInt32(m);
-		// 	if( m.skin == null )
-		// 		writeName(null);
-		// 	else
-		// 		writeSkin(m.skin);
-		// }
-
-		// out.writeInt32(d.animations.length);
-		// for( a in d.animations ) {
-		// 	writeProps(a.props);
-		// 	writeName(a.name);
-		// 	out.writeInt32(a.frames);
-		// 	writeFloat(a.sampling);
-		// 	writeFloat(a.speed);
-		// 	out.writeByte( (a.loop?1:0) | (a.events != null?2:0) );
-		// 	out.writeInt32(a.dataPosition);
-		// 	out.writeInt32(a.objects.length);
-		// 	for( o in a.objects ) {
-		// 		writeName(o.name);
-		// 		out.writeByte(o.flags.toInt());
-		// 		if( o.flags.has(HasProps) ) {
-		// 			out.writeByte(o.props.length);
-		// 			for( n in o.props )
-		// 				writeName(n);
-		// 		}
-		// 	}
-		// 	if( a.events != null ) {
-		// 		out.writeInt32(a.events.length);
-		// 		for( e in a.events ) {
-		// 			out.writeInt32(e.frame);
-		// 			writeName(e.data);
-		// 		}
-		// 	}
-		// }
 
 		var bytes = header.getBytes();
 		out = old;
 
-		// out.writeString("HMD");
-		// out.writeByte(d.version);
-		// out.writeInt32(bytes.length + 12);
 		out.write(bytes);
-		// out.writeInt32(d.data.length);
-		// out.write(d.data);
 	}
 }
