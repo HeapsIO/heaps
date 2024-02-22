26f011c7018fdff0b070b284e99d4c47bd9c2158
TothBenoit <benoit.tothdenimal@gmail.com>
2024-02-22 09:44:33 +0100

    Fix LoadingScene

--- h2d/LoadingScene.hx
+++ h2d/LoadingScene.hx
@@ -8,14 +8,20 @@ class LoadingScene extends h2d.Scene {
 		this.presentCooldown = presentCooldown;
 	}
 
-	override function render( engine : h3d.Engine ) {
-		hxd.Timer.update();
-		var dt = hxd.Timer.dt;
-		ctx.elapsedTime += dt;
-		if ( ctx.elapsedTime < presentCooldown )
+	var lastPresentTime : Float = 0.0;
+	public override function render( engine : h3d.Engine ) {
+		var time = haxe.Timer.stamp();
+		if ( time - lastPresentTime < presentCooldown)
 			return;
-		ctx.elapsedTime = 0.0;
-		hxd.System.timeoutTick();
+		lastPresentTime = time;
+
+	#if usesys
+		haxe.System.emitEvents(@:privateAccess hxd.Window.inst.event);
+	#elseif hldx
+		dx.Loop.processEvents(@:privateAccess hxd.Window.inst.onEvent);
+	#elseif hlsdl
+		sdl.Sdl.processEvents(@:privateAccess hxd.Window.inst.onEvent);
+	#end
 		super.render(engine);
 		engine.driver.present();
 	}
