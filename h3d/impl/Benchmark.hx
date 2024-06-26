HeapsIO/heaps

commit 7ac380e0cb458fe5919961be1e4b4f3e9be57116

Add measureCpuThread to Benchmark

h3d/impl/Benchmark.hx
@@ -70,6 +70,8 @@ class Benchmark extends h2d.Graphics {
 	public var measureCpu = false;
 	public var displayTriangleCount = true;
 
+	public var measureCpuThread: sys.thread.Thread = null;
+
 	var tip : h2d.Text;
 	var tipCurrent : StatsObject;
 	var tipCurName : String;
@@ -161,8 +163,15 @@ class Benchmark extends h2d.Graphics {
 			if ( displayTriangleCount )
 				tip.text += hxd.Math.fmt(engine.drawTriangles/1000000)+" Mtri";
 		}
-		else
-			tip.text = s.name+"( " + Std.int(s.time / 1e6) + "." + StringTools.lpad(""+(Std.int(s.time/1e4)%100),"0",2) + " ms " + s.drawCalls + " draws "+ dispatchS(s.dispatches) + ")";
+		else {
+			var t = s.name + "( " + Std.int(s.time / 1e6) + "." + StringTools.lpad(""+(Std.int(s.time/1e4)%100),"0",2) + " ms";
+			if (measureCpuThread == null) {
+				t += " " + s.drawCalls + " draws "+ dispatchS(s.dispatches);
+			}
+			t += ")";
+
+			tip.text = t;
+		}
 		var tw = tip.textWidth + 10;
 		var tx = s == null ? curWidth : s.xPos + ((s.xSize - tw) * .5);
 		if( tx + tw > curWidth ) tx = curWidth - tw;
@@ -170,7 +179,7 @@ class Benchmark extends h2d.Graphics {
 		if( hxd.Math.abs(tip.parent.x - tx) > 5 ) tip.parent.x = Std.int(tx);
 	}
 
-	public function begin() {
+	public function begin(withVisual=true) {
 
 		if( !enable ) return;
 
@@ -190,7 +199,7 @@ class Benchmark extends h2d.Graphics {
 		var changed = false;
 		while( waitFrames.length > 0 ) {
 			var q = waitFrames[0];
-			if( !q.isAvailable() )
+			if( measureCpuThread == null && !q.isAvailable() )
 				break;
 			waitFrames.shift();
 
@@ -250,13 +259,15 @@ class Benchmark extends h2d.Graphics {
 			changed = true;
 		}
 
-		if( allocated && visible )
-			syncVisual();
+		if (withVisual) {
+			if( allocated && visible )
+				syncVisual();
+		}
 
 		measure("begin");
 	}
 
-	function syncVisual() {
+	public function syncVisual() {
 		var s2d = getScene();
 		var old = labels;
 		labels = null;
@@ -278,7 +289,7 @@ class Benchmark extends h2d.Graphics {
 			s = s.next;
 		}
 
-		var space = 52;
+		var space = 57;
 		width -= space;
 
 		var count = 0;
@@ -332,7 +343,13 @@ class Benchmark extends h2d.Graphics {
 		time.visible = true;
 		time.textColor = 0xFFFFFF;
 		var timeMs = totalTime / 1e6;
-		time.text = Std.int(timeMs) + "." + Std.int((timeMs * 10) % 10) + (measureCpu?" cpu" : " gpu");
+		var totalName = measureCpu ? "cpu" : "gpu";
+		if (measureCpuThread != null) {
+			var n = measureCpuThread.getName();
+			if (n != null)
+				totalName = n;
+		}
+		time.text = Std.int(timeMs) + "." + Std.int((timeMs * 10) % 10) + " " + totalName;
 
 		while( labels.length > count )
 			labels.pop().remove();
@@ -389,6 +406,8 @@ class Benchmark extends h2d.Graphics {
 	public function measure( name : String ) {
 		if( !enable ) return;
 		if( currentFrame != null && currentFrame.name == name )
+			return;
+		if( measureCpuThread != null && sys.thread.Thread.current() != measureCpuThread )
 			return;
 		var q = allocQuery();
 		q.name = name;

commit 0badfa518e89eca68b8608db576ec3d52834170e

Fix Benchmark compilation error on js target
Introduced in 7ac380e0cb458fe5919961be1e4b4f3e9be57116

h3d/impl/Benchmark.hx
@@ -70,7 +70,9 @@ class Benchmark extends h2d.Graphics {
 	public var measureCpu = false;
 	public var displayTriangleCount = true;
 
+	#if target.threaded
 	public var measureCpuThread: sys.thread.Thread = null;
+	#end
 
 	var tip : h2d.Text;
 	var tipCurrent : StatsObject;
@@ -165,7 +167,10 @@ class Benchmark extends h2d.Graphics {
 		}
 		else {
 			var t = s.name + "( " + Std.int(s.time / 1e6) + "." + StringTools.lpad(""+(Std.int(s.time/1e4)%100),"0",2) + " ms";
-			if (measureCpuThread == null) {
+			#if target.threaded
+			if (measureCpuThread == null)
+			#end
+			{
 				t += " " + fmt(s.drawCalls, "draws")+ fmt(s.dispatches, "dispatches");
 			}
 			t += ")";
@@ -199,7 +204,7 @@ class Benchmark extends h2d.Graphics {
 		var changed = false;
 		while( waitFrames.length > 0 ) {
 			var q = waitFrames[0];
-			if( measureCpuThread == null && !q.isAvailable() )
+			if( #if target.threaded measureCpuThread == null && #end !q.isAvailable() )
 				break;
 			waitFrames.shift();
 
@@ -344,11 +349,13 @@ class Benchmark extends h2d.Graphics {
 		time.textColor = 0xFFFFFF;
 		var timeMs = totalTime / 1e6;
 		var totalName = measureCpu ? "cpu" : "gpu";
+		#if target.threaded
 		if (measureCpuThread != null) {
 			var n = measureCpuThread.getName();
 			if (n != null)
 				totalName = n;
 		}
+		#end
 		time.text = Std.int(timeMs) + "." + Std.int((timeMs * 10) % 10) + " " + totalName;
 
 		while( labels.length > count )
@@ -407,8 +414,10 @@ class Benchmark extends h2d.Graphics {
 		if( !enable ) return;
 		if( currentFrame != null && currentFrame.name == name )
 			return;
+		#if target.threaded
 		if( measureCpuThread != null && sys.thread.Thread.current() != measureCpuThread )
 			return;
+		#end
 		var q = allocQuery();
 		q.name = name;
 		q.drawCalls = engine.drawCalls;