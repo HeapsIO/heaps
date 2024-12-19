3dfdd6ff60f688dbd123f9785d93b7bbea691c9c
TothBenoit <benoit.tothdenimal@gmail.com>
2024-12-19 15:02:09 +0100

    Added check to see if lodConfig has a suitable culling entry.

--- h3d/prim/HMDModel.hx
+++ h3d/prim/HMDModel.hx
@@ -307,12 +307,12 @@ class HMDModel extends MeshPrimitive {
 
 		var lodConfig = getLodConfig();
 		if ( lodConfig != null ) {
-			// Culling
-			if ( screenRatio < lodConfig[lodConfig.length - 1] )
+			var lodConfigHasCulling = lodConfig.length > lodCount - 1;
+			if ( lodConfigHasCulling && screenRatio < lodConfig[lodConfig.length - 1] )
 				return lodCount;
 
 			var lodLevel : Int = 0;
-			var maxIter = ( ( lodConfig.length > lodCount - 1 ) ? lodCount - 1: lodConfig.length );
+			var maxIter = lodConfigHasCulling ? lodCount - 1 : lodConfig.length;
 			for ( i in 0...maxIter ) {
 				if ( lodConfig[i] == 0.0 )
 					return lodLevel;
