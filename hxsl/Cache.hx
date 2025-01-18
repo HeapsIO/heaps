5d7103a4deb290b1b54ff887889bb0fae0149679
Rasmus Brinck <rasmus@hacksawstudios.com>
2025-01-18 12:40:23 +0000

    fix: Remove some revert changes and apply special case for fragdepth

--- hxsl/Cache.hx
+++ hxsl/Cache.hx
@@ -1,6 +1,6 @@
 package hxsl;
-using hxsl.Ast;
 import hxsl.RuntimeShader;
+using hxsl.Ast;
 
 class BatchInstanceParams {
 
@@ -54,6 +54,11 @@ class SearchMap {
 }
 
 class Cache {
+
+	#if shader_debug_dump
+	public static var DEBUG_IDS = false;
+	public static var TRACE = true;
+	#end
 
 	var linkCache : SearchMap;
 	var linkShaders : Map<String, Shader>;
@@ -243,10 +248,12 @@ class Cache {
 
 		#if shader_debug_dump
 		var shaderId = @:privateAccess RuntimeShader.UID;
-		#if ( js && !sys )
+		var oldTrace = haxe.Log.trace;
+		#if js
 		if( shaderId == 0 ) js.Syntax.code("window.shaders = [];");
 		js.Syntax.code("window.shaders[{0}] = '';", shaderId);
-		var dbg: { writeString: String->Void, close:Void->Void } = {
+		var dbg: { writeString: String->Void, close:Void->Void } = null;
+		dbg = {
 			writeString: (str: String) -> { js.Syntax.code("window.shaders[{0}] += {1}", shaderId, str); },
 			close: () -> {}
 		};
@@ -254,13 +261,13 @@ class Cache {
 		if( shaderId == 0 ) try sys.FileSystem.createDirectory("shaders") catch( e : Dynamic ) {};
 		var dbg = sys.io.File.write("shaders/"+shaderId+"_dump.c");
 		#end
-		var oldTrace = haxe.Log.trace;
+		
 		haxe.Log.trace = function(msg,?pos) dbg.writeString(haxe.Log.formatOutput(msg,pos)+"\n");
 		if( dbg != null ) {
 			dbg.writeString("----- DATAS ----\n\n");
 			for( s in shaderDatas ) {
 				dbg.writeString("\t\t**** " + s.inst.shader.name + (s.p == 0 ? "" : " P="+s.p)+ " *****\n");
-				dbg.writeString(Printer.shaderToString(s.inst.shader,Debug.VAR_IDS)+"\n\n");
+				dbg.writeString(Printer.shaderToString(s.inst.shader,DEBUG_IDS)+"\n\n");
 			}
 		}
 		//TRACE = shaderId == 0;
@@ -293,15 +300,15 @@ class Cache {
 				checkRec(v);
 		}
 
+		#if debug
+		Printer.check(s,[for( s in shaderDatas ) s.inst.shader]);
+		#end
+
 		#if shader_debug_dump
 		if( dbg != null ) {
 			dbg.writeString("----- LINK ----\n\n");
-			dbg.writeString(Printer.shaderToString(s,Debug.VAR_IDS)+"\n\n");
+			dbg.writeString(Printer.shaderToString(s,DEBUG_IDS)+"\n\n");
 		}
-		#end
-
-		#if debug
-		Printer.check(s,[for( s in shaderDatas ) s.inst.shader]);
 		#end
 
 		var prev = s;
@@ -322,33 +329,33 @@ class Cache {
 			}
 
 
-		#if shader_debug_dump
-		if( dbg != null ) {
-			dbg.writeString("----- SPLIT ----\n\n");
-			for( s in sl )
-				dbg.writeString(Printer.shaderToString(s, Debug.VAR_IDS) + "\n\n");
-		}
-		#end
-
 		#if debug
 		for( s in sl )
 			Printer.check(s,[prev]);
 		#end
 
+		#if shader_debug_dump
+		if( dbg != null ) {
+			dbg.writeString("----- SPLIT ----\n\n");
+			for( s in sl )
+				dbg.writeString(Printer.shaderToString(s, DEBUG_IDS) + "\n\n");
+		}
+		#end
+
 		var prev = sl;
 		var sl = new hxsl.Dce().dce(sl);
+
+		#if debug
+		for( i => s in sl )
+			Printer.check(s,[prev[i]]);
+		#end
 
 		#if shader_debug_dump
 		if( dbg != null ) {
 			dbg.writeString("----- DCE ----\n\n");
 			for( s in sl )
-				dbg.writeString(Printer.shaderToString(s, Debug.VAR_IDS) + "\n\n");
+				dbg.writeString(Printer.shaderToString(s, DEBUG_IDS) + "\n\n");
 		}
-		#end
-
-		#if debug
-		for( i => s in sl )
-			Printer.check(s,[prev[i]]);
 		#end
 
 		var r = buildRuntimeShader(sl, paramVars);
@@ -358,7 +365,7 @@ class Cache {
 		if( dbg != null ) {
 			dbg.writeString("----- FLATTEN ----\n\n");
 			for( s in r.getShaders() )
-				dbg.writeString(Printer.shaderToString(s.data, Debug.VAR_IDS) + "\n\n");
+				dbg.writeString(Printer.shaderToString(s.data, DEBUG_IDS) + "\n\n");
 		}
 		#end
 
