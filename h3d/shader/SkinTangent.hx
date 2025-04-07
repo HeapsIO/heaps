5d01387907048923a50e65be25ef2db3f4b23ed0
clementlandrin <clement.landrin@hotmail.fr>
2025-04-07 12:18:02 +0200

    Temporal antialiasing using velocity on Skin meshes.

--- h3d/shader/SkinTangent.hx
+++ h3d/shader/SkinTangent.hx
@@ -4,6 +4,8 @@ class SkinTangent extends SkinBase {
 
 	static var SRC = {
 
+		@:import h3d.shader.Skin.Utils;
+
 		@input var input : {
 			var position : Vec3;
 			var normal : Vec3;
@@ -13,30 +15,38 @@ class SkinTangent extends SkinBase {
 		};
 
 		var transformedTangent : Vec4;
+		var previousTransformedPosition : Vec3;
 
 		function vertex() {
-			transformedPosition =
-				(relativePosition * bonesMatrixes[int(input.indexes.x)]) * input.weights.x +
-				(relativePosition * bonesMatrixes[int(input.indexes.y)]) * input.weights.y +
-				(relativePosition * bonesMatrixes[int(input.indexes.z)]) * input.weights.z;
-			transformedNormal = 
-				(input.normal * mat3(bonesMatrixes[int(input.indexes.x)])) * input.weights.x +
-				(input.normal * mat3(bonesMatrixes[int(input.indexes.y)])) * input.weights.y +
-				(input.normal * mat3(bonesMatrixes[int(input.indexes.z)])) * input.weights.z;
-			transformedTangent.xyz =
-				(input.tangent.xyz * mat3(bonesMatrixes[int(input.indexes.x)])) * input.weights.x +
-				(input.tangent.xyz * mat3(bonesMatrixes[int(input.indexes.y)])) * input.weights.y +
-				(input.tangent.xyz * mat3(bonesMatrixes[int(input.indexes.z)])) * input.weights.z;
+			if ( calcPrevPos ) {
+				boneMatrixX = prevBonesMatrixes[int(input.indexes.x)];
+				boneMatrixY = prevBonesMatrixes[int(input.indexes.y)];
+				boneMatrixZ = prevBonesMatrixes[int(input.indexes.z)];
+				boneMatrixW = prevBonesMatrixes[int(input.indexes.w)];
+				skinWeights = vec4(input.weights, 0.0);
+				if ( fourBonesByVertex )
+					skinWeights.w = 1 - (input.weights.x + input.weights.y + input.weights.z);
 
-			if(fourBonesByVertex) {
-				var w4 = 1 - (input.weights.x + input.weights.y + input.weights.z);
-				transformedPosition += (relativePosition * bonesMatrixes[int(input.indexes.w)]) * w4;
-				transformedNormal += (input.normal * mat3(bonesMatrixes[int(input.indexes.z)])) * w4;
-				transformedTangent.xyz += (input.tangent.xyz * mat3(bonesMatrixes[int(input.indexes.w)])) * w4;
+				previousTransformedPosition = applySkinPoint(relativePosition);
 			}
-			
+
+			boneMatrixX = bonesMatrixes[int(input.indexes.x)];
+			boneMatrixY = bonesMatrixes[int(input.indexes.y)];
+			boneMatrixZ = bonesMatrixes[int(input.indexes.z)];
+			boneMatrixW = bonesMatrixes[int(input.indexes.w)];
+			skinWeights = vec4(input.weights, 0.0);
+			if ( fourBonesByVertex )
+				skinWeights.w = 1 - (input.weights.x + input.weights.y + input.weights.z);
+
+			transformedPosition = applySkinPoint(relativePosition);
+			transformedNormal = applySkinVec(input.normal);
+			transformedTangent.xyz = applySkinVec(input.tangent.xyz);
+
 			transformedNormal = normalize(transformedNormal);
 			transformedTangent.xyz = normalize(transformedTangent.xyz);
+
+			if ( !calcPrevPos )
+				previousTransformedPosition = transformedPosition;
 		}
 
 	};
