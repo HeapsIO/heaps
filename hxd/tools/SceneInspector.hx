ed90f376f3beea459aea60e1ae0db4e1c26209f5
Yuxiao Mao <yuxiao.mao@outlook.com>
2025-05-02 15:09:38 +0200

    SceneInspector: basic 3d and 2d inspect

---
+++ hxd/tools/SceneInspector.hx
@@ -0,0 +1,315 @@
+package hxd.tools;
+
+#if domkit
+class SceneInspectorBaseComp extends h2d.Flow implements h2d.domkit.Object {
+}
+
+@:uiInitFunction(init)
+class SceneInspectorBaseDynComp extends h2d.Flow implements h2d.domkit.Object {
+	function new(?parent) {
+		super(parent);
+		init();
+	}
+
+	function init() {
+		initComponent();
+	}
+
+	public function rebuild() {
+		removeChildren();
+		init();
+	}
+}
+
+class SceneInspectorButtonComp extends SceneInspectorBaseComp {
+	static var SRC = <scene-inspector-button-comp content-align="middle">
+		<text text={text}/>
+	</scene-inspector-button-comp>
+
+	var text : String;
+	var enable : Bool = true;
+
+	public function new( text : String, ?parent ) {
+		this.text = text;
+		super(parent);
+		initComponent();
+		enableInteractive = true;
+		interactive.enableRightButton = true;
+		interactive.onClick = function(e) {
+			if( e.button == 0 && enable )
+				onClick();
+			else if( e.button == 1 && enable )
+				onRightClick();
+		};
+		interactive.onOver = function(_) {
+			dom.hover = true;
+		};
+		interactive.onOut = function(_) {
+			dom.hover = false;
+		};
+	}
+
+	public dynamic function onClick() { }
+
+	public dynamic function onRightClick() { }
+}
+
+class SceneInspectorObjectComp extends SceneInspectorBaseDynComp {
+	static var SRC = <scene-inspector-object-comp layout="vertical">
+		<flow class="obj-header" layout="horizontal" padding="5" hspacing="5"
+			alpha={isVisible() ? 1 : 0.5}
+		>
+			<scene-inspector-button-comp(expanded ? "-" : "+")
+				class="expand-btn" width="20" background="#4287f5"
+				onClick={toggleExpand}
+			/>
+			<flow class="obj-line" hspacing="5">
+				<text class="obj-name" text={getName()}/>
+				<text class="obj-desc" text={getDesc()}/>
+			</flow>
+			<scene-inspector-button-comp("visible")
+				class="visible-btn" background="#4287f5"
+				onClick={toggleVisibility}
+			/>
+			<scene-inspector-button-comp("teleport")
+				class="teleport-btn" background="#4287f5"
+				onClick={teleportTo}
+			if(is3d)/>
+		</flow>
+	</scene-inspector-object-comp>
+
+	var inspector : SceneInspector;
+	var expanded : Bool;
+	var is3d : Bool;
+
+	public function new( inspector : SceneInspector, ?parent ){
+		this.inspector = inspector;
+		super(parent);
+	}
+
+	function getName() : String {
+		return "[Object]";
+	}
+
+	function getDesc() : String {
+		return "";
+	}
+
+	function toggleExpand() {
+		expanded = !expanded;
+		rebuild();
+	}
+
+	function isVisible() : Bool {
+		return true;
+	}
+
+	function toggleVisibility() {
+	}
+
+	function teleportTo() {
+	}
+
+}
+
+class SceneInspectorObject2dComp extends SceneInspectorObjectComp {
+	static var SRC = <scene-inspector-object2d-comp layout="vertical">
+		${if( expanded ){
+			<flow class="child-list" layout="vertical" padding-left="20">
+				for( child in obj ) {
+					<scene-inspector-object2d-comp(inspector, child)/>
+				}
+			</flow>
+		}}
+	</scene-inspector-object2d-comp>
+
+	var obj : h2d.Object;
+
+	public function new( inspector : SceneInspector, obj : h2d.Object, ?parent ){
+		this.obj = obj;
+		this.expanded = obj.numChildren == 0;
+		this.is3d = false;
+		super(inspector, parent);
+	}
+
+	override function getName() {
+		var objName = obj.name ?? "Object";
+		return '[$objName]';
+	}
+
+	override function getDesc() {
+		var typeName = Type.getClassName(Type.getClass(obj));
+		var childCount = getChildCountRec(obj);
+		return '$typeName (child:$childCount)';
+	}
+
+	override function toggleExpand() {
+		// prevent expand self
+		if( obj == @:privateAccess inspector.rootComp )
+			return;
+		super.toggleExpand();
+	}
+
+	override function isVisible() {
+		var p = obj;
+		while( p != null ){
+			if( !p.visible )
+				return false;
+			p = p.parent;
+		}
+		return true;
+	}
+
+	override function toggleVisibility() {
+		// do not make parent or self invisible
+		@:privateAccess if( obj == inspector.parent || obj == inspector.rootComp )
+			return;
+		obj.visible = !obj.visible;
+		// update might changes visible flag
+		haxe.Timer.delay(() -> rebuild(), 0);
+	}
+
+	static function getChildCountRec( obj : h2d.Object ) : Int{
+		var count = 0;
+		for( child in obj )
+			count += getChildCountRec(child);
+		return obj.numChildren + count;
+	}
+}
+
+class SceneInspectorObject3dComp extends SceneInspectorObjectComp {
+	static var SRC = <scene-inspector-object3d-comp layout="vertical">
+		${if( expanded ){
+			<flow class="child-list" layout="vertical" padding-left="20">
+				for( child in obj ) {
+					<scene-inspector-object3d-comp(inspector, child)/>
+				}
+			</flow>
+		}}
+	</scene-inspector-object3d-comp>
+
+	var obj : h3d.scene.Object;
+
+	public function new( inspector : SceneInspector, obj : h3d.scene.Object, ?parent ){
+		this.obj = obj;
+		this.expanded = obj.numChildren == 0;
+		this.is3d = true;
+		super(inspector, parent);
+	}
+
+	override function getName() {
+		var objName = obj.name ?? "Object";
+		return '[$objName]';
+	}
+
+	override function getDesc() {
+		var typeName = Type.getClassName(Type.getClass(obj));
+		var childCount = getChildCountRec(obj);
+		var triangleCount = getTriangleCount(obj);
+		return '$typeName (child:$childCount) (tri:$triangleCount)';
+	}
+
+	override function isVisible() {
+		var p = obj;
+		while( p != null ){
+			if( !p.visible )
+				return false;
+			p = p.parent;
+		}
+		return true;
+	}
+
+	override function toggleVisibility() {
+		obj.visible = !obj.visible;
+		// setter might changes visible flag
+		haxe.Timer.delay(() -> rebuild(), 0);
+	}
+
+	override function teleportTo() {
+		inspector.teleportTo(obj);
+	}
+
+	static function getChildCountRec( obj : h3d.scene.Object ) : Int{
+		var count = 0;
+		for( child in obj )
+			count += getChildCountRec(child);
+		return obj.numChildren + count;
+	}
+
+	static function getVisibleMeshes( obj: h3d.scene.Object, ?out : Array<h3d.scene.Mesh> ){
+		if( out == null ) out = [];
+		if( obj.visible ){
+			var m = Std.downcast(obj, h3d.scene.Mesh);
+			if( m != null ) out.push(m);
+			for( c in @:privateAccess obj.children )
+				getVisibleMeshes(c, out);
+		}
+		return out;
+	}
+
+	static function getTriangleCount( obj: h3d.scene.Object ) : Int{
+		var tri = 0;
+		for( m in getVisibleMeshes(obj) ){
+			tri += m.primitive.triCount();
+		}
+		return tri;
+	}
+}
+
+class SceneInspectorComp extends SceneInspectorBaseComp {
+	static var SRC = <scene-inspector-comp
+	 	layout="vertical" overflow="scroll"
+	 	padding="5" max-height="720"
+		background="#555555" background-alpha="0.8"
+	>
+		<scene-inspector-object3d-comp(inspector, root3d)/>
+		<scene-inspector-object2d-comp(inspector, root2d) if(root2d != null)/>
+	</scene-inspector-comp>
+
+	var inspector : SceneInspector;
+
+	public function new( inspector : SceneInspector, root3d : h3d.scene.Object, root2d : Null<h2d.Object>, ?parent ) {
+		this.inspector = inspector;
+		super(parent);
+		initComponent();
+	}
+}
+
+/**
+	Requires:
+	`-lib domkit --macro domkit.Macros.registerComponentsPath("$")`.
+
+	Inspect `h3d.scene.Object` (usually `s3d`) and `h2d.Object` (usually `s2d`) with domkit-based UI.
+**/
+class SceneInspector {
+	public var style(default, null) : h2d.domkit.Style;
+	var parent : h2d.Object;
+	var s3d : h3d.scene.Object;
+	var s2d : h2d.Object;
+	var rootComp : SceneInspectorComp;
+
+	public function new( parent : h2d.Object, s3d : h3d.scene.Object, ?s2d : h2d.Object ) {
+		style = new h2d.domkit.Style();
+		this.parent = parent;
+		this.s3d = s3d;
+		this.s2d = s2d;
+		rootComp = new SceneInspectorComp(this, s3d, s2d, parent);
+		style.addObject(rootComp);
+	}
+
+	public function dispose() {
+		style = null;
+		if( parent != null ) {
+			parent.removeChild(rootComp);
+		}
+		parent = null;
+		s3d = null;
+		s2d = null;
+		rootComp = null;
+		teleportTo = function(obj) {};
+	}
+
+	public dynamic function teleportTo( obj : h3d.scene.Object ) {
+	}
+}
+#end
