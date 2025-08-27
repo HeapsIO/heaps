133382fab0106af78d173d6cd6a5ac78da6036e7
Leonardo Jeanteur <leonardo@shirogames.com>
2025-08-27 08:54:00 +0200

    Revert "CascadeShadowMap : Do not use border"
    This reverts commit ca6ce54100eb7270de0be3f2631519e3c3840267.

--- h3d/pass/CascadeShadowMap.hx
+++ h3d/pass/CascadeShadowMap.hx
@@ -47,7 +47,6 @@ class CascadeShadowMap extends DirShadowMap {
 		super(light);
 		format = R32F;
 		shader = dshader = cshader = new h3d.shader.CascadeShadow();
-		border = null;
 	}
 
 	public override function getShadowTex() {
