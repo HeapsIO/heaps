d901273d581470b92d5513f68dc44146348b2cc7
Leo Bergman <leo@hacksawstudios.com>
2025-10-08 10:55:01 +0000

    chore: Remove duplicate imports

--- h3d/impl/GlDriver.hx
+++ h3d/impl/GlDriver.hx
@@ -1,8 +1,6 @@
 package h3d.impl;
 import h3d.impl.Driver;
 import h3d.mat.Data;
-import h3d.mat.Data;
-import h3d.mat.Data;
 import h3d.mat.Pass;
 import h3d.mat.Stencil;
 import hxd.CompressedTextureFormat;
