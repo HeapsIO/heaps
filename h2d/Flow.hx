60ec6c3480795a218ba39accc6bb7631a9a332de
k <k@klabz.org>
2025-11-24 17:11:59 +0100

    [Flow] Fix line height of first line in multiline flow

--- h2d/Flow.hx
+++ h2d/Flow.hx
@@ -1426,6 +1426,7 @@ class Flow extends Object {
 			});
 
 			// position all not absolute nodes
+			maxLineHeight = 0;
 			forChildren(function(i, p, c) {
 				if( p.autoSizeWidth != null || p.autoSizeHeight != null )
 					calcSize(p, c);
