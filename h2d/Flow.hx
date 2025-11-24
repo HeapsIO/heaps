e6ccc30e22c60abacfd3139833b40341a63785c0
k <k@klabz.org>
2025-11-24 20:12:59 +0100

    Same for vertical layout

--- h2d/Flow.hx
+++ h2d/Flow.hx
@@ -1625,6 +1625,7 @@ class Flow extends Object {
 			});
 
 			// position all not absolute nodes
+			maxColWidth = 0;
 			forChildren(function(i, p, c) {
 				if( p.autoSizeWidth != null || p.autoSizeHeight != null )
 					calcSize(p, c);
