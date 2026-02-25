5270e4da8b7f855ea3d147ac2a89dc88e1872bd3
Clément Espeute <espeut.clement@gmail.com>
2026-02-25 10:43:06 +0100

    [dx] Add dx12_shader_debug compile flag

--- h3d/impl/DX12Driver.hx
+++ h3d/impl/DX12Driver.hx
@@ -1225,7 +1225,7 @@ class DX12Driver extends h3d.impl.Driver {
 		return null;
 	}
 
-	static final SHADER_ARGS : Array<String>= [/*"-Zi", "-Qembed_debug"*/];
+	static final SHADER_ARGS : Array<String>= [#if dx12_shader_debug "-Zi", "-Qembed_debug" #end];
 	function compileSource( sh : hxsl.RuntimeShader.RuntimeShaderData, profile, rootStr = "" ) {
 		var out = new hxsl.HlslOut();
 		if( sh.code == null ) {
