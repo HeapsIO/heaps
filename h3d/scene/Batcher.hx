4e05439e481c105e84ecf262e12154226581e760
julesvannier <julesvannier44@gmail.com>
2026-03-30 11:09:17 +0200

    Batcher shadow culling offset (reduce shadow popping)

--- h3d/scene/Batcher.hx
+++ h3d/scene/Batcher.hx
@@ -85,6 +85,7 @@ class BatchGroup {
 class Batcher extends h3d.scene.Object {
 	public var shadowMaxDistance = -1.0;
 	public var shadowCameraFrustumCulling = false; // Aggressive culling inside Shadow passes, using player camera frustum
+	public var shadowCullingOffset = 50.0;
 	public var isRelative : Bool = false;
 	public var batchFlags = new haxe.EnumFlags<BatcherFlags>();
 	public var syncShader : SyncShaderInterface;
@@ -940,6 +941,7 @@ private class BatchPass {
 							tmpMaxLs.max(pLs);
 						}
 
+						tmpMinLs.z = tmpMinLs.z - batcher.shadowCullingOffset;
 						builderShader.lightMatrix = lightMatrix;
 						builderShader.lightOBBMin.set(tmpMinLs.x, tmpMinLs.y, tmpMinLs.z);
 						builderShader.lightOBBMax.set(tmpMaxLs.x, tmpMaxLs.y, tmpMaxLs.z);
