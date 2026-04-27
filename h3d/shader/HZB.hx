6c03fec7c3ee3d516a9af09e8465e666b0525037
julesvannier <julesvannier44@gmail.com>
2026-04-27 16:40:02 +0200

    HZB better implementation

--- h3d/shader/HZB.hx
+++ h3d/shader/HZB.hx
@@ -6,29 +6,32 @@ class HZB extends h3d.shader.ScreenShader {
 		@param var source : Sampler2D;
 		@param var sourceWidth : Int;
 		@param var sourceHeight : Int;
+		var depth : Float;
+		var prevTexelPos : IVec2;
 
-		function compareDepth( depth1 : Float, depth2 : Float ) : Float {
-			return compareMax ? max(depth1, depth2) : min(depth1, depth2);
+		function compareDepth( offset : IVec2 ) {
+			var depth2 = source.fetch(prevTexelPos + offset).x;
+			depth = compareMax ? max(depth, depth2) : min(depth, depth2);
 		}
 
 		function fragment() {
-			var prevTexelPos = floor(fragCoord.xy) * 2.0;
-			var depth = source.fetch(ivec2(prevTexelPos)).x;
-			depth = compareDepth(depth, source.fetch(ivec2(prevTexelPos + vec2(1, 0))).x);
-			depth = compareDepth(depth, source.fetch(ivec2(prevTexelPos + vec2(0, 1))).x);
-			depth = compareDepth(depth, source.fetch(ivec2(prevTexelPos + vec2(1, 1))).x);
+			prevTexelPos = ivec2(floor(fragCoord.xy) * 2.0);
+			depth = source.fetch(prevTexelPos).x;
+			compareDepth(ivec2(1, 0));
+			compareDepth(ivec2(0, 1));
+			compareDepth(ivec2(1, 1));
 			var prevWidthOdd = (sourceWidth & 1) != 0;
 			var prevHeightOdd = (sourceHeight & 1) != 0;
 			if ( prevWidthOdd ) {
-				depth = compareDepth(depth, source.fetch(ivec2(prevTexelPos + vec2(2, 0))).x);
-				depth = compareDepth(depth, source.fetch(ivec2(prevTexelPos + vec2(2, 1))).x);
+				compareDepth(ivec2(2, 0));
+				compareDepth(ivec2(2, 1));
 			}
 			if ( prevHeightOdd ) {
-				depth = compareDepth(depth, source.fetch(ivec2(prevTexelPos + vec2(0, 2))).x);
-				depth = compareDepth(depth, source.fetch(ivec2(prevTexelPos + vec2(1, 2))).x);
+				compareDepth(ivec2(0, 2));
+				compareDepth(ivec2(1, 2));
 			}
 			if ( prevWidthOdd && prevHeightOdd )
-				depth = compareDepth(depth, source.fetch(ivec2(prevTexelPos + vec2(2, 2))).x);
+				compareDepth(ivec2(2, 2));
 
 			output.color = vec4(depth.xxx, 1.0);
 		}
