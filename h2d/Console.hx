41daf45c636121ddb3ca9a261149ea78a83abba9
ncannasse <ncannasse@gmail.com>
2026-05-02 10:11:09 +0200

    fixed height with fontgroup

--- h2d/Console.hx
+++ h2d/Console.hx
@@ -103,8 +103,8 @@ class Console #if !macro extends h2d.Object #end {
 	**/
 	public function new(font:h2d.Font,?parent) {
 		super(parent);
-		height = Math.ceil(font.lineHeight) + 2;
 		logTxt = new h2d.HtmlText(font, this);
+		height = Math.ceil(logTxt.font.lineHeight) + 2;
 		logTxt.x = 2;
 		logTxt.dropShadow = { dx : 0, dy : 1, color : 0, alpha : 0.5 };
 		logTxt.visible = false;
